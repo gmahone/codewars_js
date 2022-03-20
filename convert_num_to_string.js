aa6f55a192b889552fb4062c1a9c214f17049033
gmahone <gmahone@gmail.com>
2022-03-20 21:26:59 +0100

    add other solutions

--- convert_num_to_string.js
+++ convert_num_to_string.js
@@ -2,3 +2,13 @@ numToStringConvert = function(num){
     return(String(num));
 }
 
+/* add other solutions */
+
+function numberToString(num) {
+  return num.toString();
+}
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return String(num);
+}
