216a322be1991e406735aa32bfd1449153e52783
gmahone <gmahone@gmail.com>
2022-03-21 21:05:10 +0100

    add other solutions

--- sum_without_min_or_max.js
+++ sum_without_min_or_max.js
@@ -18,3 +18,37 @@ function sumArray(array) {
     }
     return(sum - min - max);
   }
+
+
+
+ /* other solutions */
+  function sumArray(array) {
+    if (array == null) {
+      return 0;
+    } else if (array.length < 2) {
+      return 0;
+    } else {
+      array = array.sort(function(a,b) {return a - b;});
+      var total = 0;
+      for (var i = 1; i < array.length - 1; i++) {
+        total += array[i];
+      }
+      return total;
+    }
+  }
+
+
+  function sumArray(array) {
+    return Array.isArray(array) && array.length > 1
+      ? array.reduce((s, n) => s + n, 0) - Math.min(...array) - Math.max(...array)
+      : 0
+  }
+
+  const sumArray = (numbers) => (
+    numbers && numbers.length > 1
+      ? numbers
+        .sort((a, b) => a - b)
+        .slice(1, -1)
+        .reduce((sum, number) => sum + number, 0)
+      : 0
+  );
