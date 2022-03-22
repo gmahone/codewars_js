fe235e224aa836c149bc3d3041de717e3b0bd349
gmahone <gmahone@gmail.com>
2022-03-22 13:26:08 +0100

    add other solutions

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -12,3 +12,31 @@ function bmiConverter(weight, height){
     }
 }
 
+/* other solutions */
+
+const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                                    bmi <= 25 ? "Normal" :
+                                    bmi <= 30 ? "Overweight" : "Obese";
+
+function bmi(weight, height) {
+  
+var bmi  = weight/(height*height);
+
+ return bmi < 18.5 ? "Underweight" : bmi <=25 ? "Normal" : bmi <= 30 ? "Overweight" : "Obese";
+
+}
+
+function bmi(weight, height) {
+  var formula = (weight / Math.pow(height, 2));
+  switch (true) {
+    case formula <=18.5:
+    return 'Underweight';
+    case formula <=25.0:
+    return 'Normal';
+    case formula <=30:
+    return 'Overweight';
+    default:
+    return 'Obese';
+    
+  }
+}
