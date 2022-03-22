ee59b2e8496f50bf7a449782f3c7d2136787907c
gmahone <gmahone@gmail.com>
2022-03-22 13:40:27 +0100

    add other solutions

--- array_includes_x.js
+++ array_includes_x.js
@@ -1,3 +1,11 @@
 function arrayContainsX(array, x){
     return array.includes(x);
 }
+
+
+/* other solutions */
+const check = (a,x) => a.includes(x);
+
+function check(a,x){
+  return a.indexOf(x) > -1 ? true : false;
+};
