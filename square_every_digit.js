10a5d9465306c1c4cfc56f8ac502f5946ae7997b
gmahone <gmahone@gmail.com>
2022-03-23 10:18:15 +0100

    add other solutions

--- square_every_digit.js
+++ square_every_digit.js
@@ -6,3 +6,21 @@ function squareDigits(num){
   }
   return Number(out);
 }
+
+
+/* other solutions */
+function squareDigits(num){
+  return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
+}
+
+function squareDigits(num){
+  var array = num.toString().split('').map( function(int) {
+    var i = parseInt(int);
+    return i * i;
+  });
+  return parseInt(array.join(""));
+}
+
+function squareDigits(num){
+  return +num.toString().split('').map(i => i*i).join('');
+}
