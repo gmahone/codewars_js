fc8488a8cfc893465cf3983b553ecec1163306e0
gmahone <gmahone@gmail.com>
2022-03-24 21:55:08 +0100

    add other solutions

--- dna_complement.js
+++ dna_complement.js
@@ -5,3 +5,24 @@ function DNAStrand(dna){
   dna = dna.replace(/A/g, "t");
   return dna.toUpperCase();
 }
+
+// add other solutions
+function DNAStrand(dna) {
+  return dna.replace(/./g, function(c) {
+    return DNAStrand.pairs[c]
+  })
+}
+DNAStrand.pairs = {
+  A: 'T',
+  T: 'A',
+  C: 'G',
+  G: 'C',
+}
+
+var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+function DNAStrand(dna){
+  return dna.split('').map(function(v){ return pairs[v] }).join('');
+}
+
+let pairs = {A:'T',T:'A',C:'G',G:'C'};
+const DNAStrand = dna => dna.replace(/./g, c => pairs[c]);
