ae973fa597e0a2f4e5eff99eb576fe5b383c08e4
gmahone <gmahone@gmail.com>
2022-03-25 14:27:20 +0100

    add other solutions

--- sum_two_smallest_numbers.js
+++ sum_two_smallest_numbers.js
@@ -3,3 +3,9 @@ function sumTwoSmallestNumbers(numbers) {
   return(numbers[0] + numbers[1])
 }
 
+// other solutions
+
+function sumTwoSmallestNumbers(numbers) {  
+  var [ a, b ] = numbers.sort((a, b) => a - b)
+  return a + b
+}
