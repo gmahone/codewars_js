74319179bcb0bfda2b4f6e43432b934d420a17f8
gmahone <gmahone@gmail.com>
2022-03-25 11:06:21 +0100

    added other solutions

--- factorial.js
+++ factorial.js
@@ -13,3 +13,28 @@ function factorial(n){
     }
     return out;
   }
+
+// other solutions
+
+function factorial(n) {
+  if (n < 0 || n > 12)
+    throw new RangeError();
+  return n <= 1 ? 1 : n * factorial(n - 1);
+}
+
+function factorial(n){
+
+ if (n < 0 || n > 12) {
+    throw new RangeError('Parameter must be between ' + 0 + ' and ' + 12);
+  }
+return n<2 ? 1 : n*factorial(n-1);
+}
+
+function factorial(n) {
+  if (n < 0 || n > 12)
+    throw RangeError();
+  var f = 1;
+  while (n > 1)
+    f *= n--;
+  return f;
+}
