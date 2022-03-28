c43f5bfcb5586fbf4a4f9ac139a8eb42b5d92b8f
gmahone <gmahone@gmail.com>
2022-03-28 10:18:20 +0200

    add other solutions

--- testing_1-2-3.js
+++ testing_1-2-3.js
@@ -1,3 +1,20 @@
 var number=function(array){
   return array.map( (a,b) => `${b+1}: ${a}`);
 }
+
+
+// other solutions
+
+var number = function(array) {
+  return array.map(function (line, index) {
+    return (index + 1) + ": " + line;
+  });
+}
+
+let number = (a) => a.map((v, i) => `${i + 1}: ${v}`)
+
+const number = array => array.map((n, i) => `${i + 1}: ${n}`);
+
+function number(arr) {
+  return arr.map((e, i) => `${++i}: ${e}`);
+}
