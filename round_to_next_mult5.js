eb6e0ad7ff3a0a7afb59dd6f22d31e5e04f2f6d1
gmahone <gmahone@gmail.com>
2022-03-28 17:04:36 +0200

    add other solutions

--- round_to_next_mult5.js
+++ round_to_next_mult5.js
@@ -1,3 +1,16 @@
 function roundToNext5(n){
   return n % 5 ? (Math.floor( n/5 ) + 1) * 5 : n
 }
+
+
+// other solutions
+function roundToNext5(n){
+  return Math.ceil(n/5)*5;
+}
+
+const roundToNext5 = n => Math.ceil(n / 5) * 5;
+
+function roundToNext5(n){
+  while(n % 5 !== 0) n++;
+  return n;
+}
