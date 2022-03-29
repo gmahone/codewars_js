06be6cd3dd1f3eedc8f405b8a203054abb21b7ff
gmahone <gmahone@gmail.com>
2022-03-29 13:03:28 +0200

    add other solutions

--- anagram_detector.js
+++ anagram_detector.js
@@ -2,3 +2,33 @@ var isAnagram = function(test, original) {
   return test.toUpperCase().split("").sort().reduce( (acc,c) => acc + c) === 
     original.toUpperCase().split("").sort().reduce( (acc,c) => acc + c);
 };
+
+
+// other solutions
+
+// write the function isAnagram
+var isAnagram = function(test, original) {
+  var t = test.toLowerCase().split('').sort().join('');
+  var o = original.toLowerCase().split('').sort().join('');
+  return (t==o)?true:false;
+};
+
+String.prototype.sortLetters = function() {
+  return this.toLowerCase().split('').sort().join('');
+}
+
+var isAnagram = function(test, original) {
+  return test.sortLetters() == original.sortLetters();
+};
+
+function isAnagram (test, original) {
+  return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("");
+}
+
+function isAnagram(str1, str2){
+    return sortWord(str1) == sortWord(str2);
+}
+
+function sortWord(word){
+    return word.toLowerCase().split("").sort().join("");
+}
