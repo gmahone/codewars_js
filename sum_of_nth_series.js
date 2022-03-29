b9f1ae0251d68a63dec106ae4afd4a84d87ec3f3
gmahone <gmahone@gmail.com>
2022-03-29 17:17:30 +0200

    add other solutions

--- sum_of_nth_series.js
+++ sum_of_nth_series.js
@@ -6,3 +6,30 @@ function SeriesSum(n){
       .toFixed(2)
       .toString()
 }
+
+
+// other solutions
+
+function SeriesSum(n) {
+  for (var s = 0, i = 0; i < n; i++) {
+    s += 1 / (1 + i * 3)
+  }
+  
+  return s.toFixed(2)
+}
+
+function SeriesSum(n)
+{
+  for(a=0,i=1;i<=n*3;i+=3) a+=1/i;
+  return a.toFixed(2);
+}
+
+function SeriesSum(n, s = 0) {
+    return n ? SeriesSum(n - 1, s + 1 / (3 * n - 2)) : s.toFixed(2)
+}
+
+function SeriesSum(n) {
+  return Array(n).fill(0).map((e, i) => 3 * i + 1).reduce((s, e) => s + 1 / e, 0).toFixed(2);
+}
+
+SeriesSum = n => Array.from(Array(n), (e, i) => 1 / (i * 3 + 1)).reduce((s, e) => s + e, 0).toFixed(2);
