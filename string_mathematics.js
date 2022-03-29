aa351f4bcbd193343429642c60c5196c5b8f57c2
gmahone <gmahone@gmail.com>
2022-03-29 10:23:13 +0200

    add other solutions

--- string_mathematics.js
+++ string_mathematics.js
@@ -8,3 +8,31 @@ const subStringMath = {
 function arithmetic(a, b, operator){
   return subStringMath[operator.toLowerCase()](Number(a), Number(b));
 }
+
+
+// add other solutions
+
+function arithmetic(a, b, operator){
+  switch(operator) {
+    case 'add':
+      return a + b;
+    case 'subtract':
+      return a - b;
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      return a / b;
+  }
+}
+
+const arithmetic = (a, b, operator) => ({
+  'add'     : a + b,
+  'subtract': a - b,
+  'multiply': a * b,
+  'divide'  : a / b
+}[operator]);
+
+function arithmetic(a, b, operator){
+  optable = { "add":"+", "subtract": "-", "multiply": "*" , "divide":"/"};
+  return eval(a + optable[operator] + b); 
+}
