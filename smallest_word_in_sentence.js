c36ee4fb7d261d183441bcb23771c5bd7ed79df5
gmahone <gmahone@gmail.com>
2022-03-29 09:36:32 +0200

    add other solutions

--- smallest_word_in_sentence.js
+++ smallest_word_in_sentence.js
@@ -2,3 +2,23 @@ function findShort(s){
   return s.split(" ").map( a => a.length ).sort( (a,b) => a - b)[0];
 }
 
+
+// other solutions
+
+function findShort(s){
+  return Math.min.apply(null, s.split(' ').map(w => w.length));
+}
+
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
+
+const findShort = (s) => s
+  .split(' ')
+  .sort((a, b) => b.length - a.length)
+  .pop()
+  .length;
+
+function findShort(s) {
+  return s.split(' ').reduce((min, word) => Math.min(min, word.length), Infinity);
+}
