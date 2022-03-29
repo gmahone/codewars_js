86160514682310226aa2579a8713b49b33ae80c9
gmahone <gmahone@gmail.com>
2022-03-29 22:21:29 +0200

    add an additional forEach solution

--- detect_printer_error.js
+++ detect_printer_error.js
@@ -11,3 +11,14 @@ const printerError = s => `${s.replace(/[a-m]/gi, "").length}/${s.length}`;
 const printerError = ($) => [ ($.match(/[n-z]/g) || []).length, $.length ].join('/')
 
 var printerError = s => (s.match(/[n-z]/g) || []).length + '/' + s.length;
+
+function printerError(s) {
+  var allowed = 'abcdefghijklm';
+  var error = 0;
+
+  s.split('').forEach( function( item ) {
+    return ( allowed.indexOf( item ) === -1 ) ? error++ : '';
+  });
+  
+  return error + '/' + s.length;
+}
