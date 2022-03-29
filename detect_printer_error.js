d0f7c0d9620b99474a553dddd49e74669d7a2b01
gmahone <gmahone@gmail.com>
2022-03-29 22:18:47 +0200

    add other solutions

--- detect_printer_error.js
+++ detect_printer_error.js
@@ -3,3 +3,11 @@ function printerError(s) {
   let total = s.length;
   return `${error}/${total}`;
 }
+
+
+// add other solutions
+const printerError = s => `${s.replace(/[a-m]/gi, "").length}/${s.length}`;
+
+const printerError = ($) => [ ($.match(/[n-z]/g) || []).length, $.length ].join('/')
+
+var printerError = s => (s.match(/[n-z]/g) || []).length + '/' + s.length;
