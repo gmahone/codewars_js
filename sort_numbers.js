9dcbf6ae2bc6525543f78b330a4d800f36fdc8b8
gmahone <gmahone@gmail.com>
2022-03-30 13:20:36 +0200

    add other solutions

--- sort_numbers.js
+++ sort_numbers.js
@@ -1,3 +1,21 @@
 function solution(nums){
   return nums ? nums.sort( (a,b) => a - b) : [];
 }
+
+
+// other solutions
+function solution(nums){
+  return (nums || []).sort(function(a, b){
+    return a - b
+  });
+}
+
+function solution(nums){
+    return nums !== null ? nums.sort(function(a,b){return a-b}) : [];
+}
+
+let solution = (nums) => (nums || []).sort((a,b) => (a-b))
+
+function solution(nums){
+  return Array.isArray(nums) ? nums.sort(function(a, b) { return a - b; }) : [];
+}
