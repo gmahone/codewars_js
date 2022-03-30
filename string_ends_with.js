e3ec27575cdac389b4e90de9eabbd9484ae1ce22
gmahone <gmahone@gmail.com>
2022-03-30 13:55:18 +0200

    add other solutions

--- string_ends_with.js
+++ string_ends_with.js
@@ -1,3 +1,20 @@
 function solution(str, ending){
   return str.substring(str.length - ending.length).includes(ending);
 }
+
+
+// other solutions
+
+function solution(str, ending){
+  return str.endsWith(ending);
+}
+
+const solution = (str, ending) => str.endsWith(ending);
+
+function solution(str, ending){
+  return new RegExp(ending+"$", "i").test(str);
+}
+
+function solution(str, ending){
+  return str.substr(-ending.length) == ending;
+}
