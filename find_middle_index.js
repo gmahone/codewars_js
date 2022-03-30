bd10b262cc3852475a9a7e036992fceff20662be
gmahone <gmahone@gmail.com>
2022-03-30 20:46:36 +0200

    add other solutions

--- find_middle_index.js
+++ find_middle_index.js
@@ -1,1 +1,14 @@
 const gimme = triplet => triplet.indexOf([...triplet].sort( (a,b) => a - b )[1])
+
+
+// other solutions
+
+function gimme(a) {
+  return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+}
+
+var gimme = function (inputArray) {
+  return inputArray.indexOf(inputArray.slice(0).sort(function(a,b) { return a-b; })[1]);
+};
+
+var gimme=arr =>arr.map((x,i) => [x,i]).sort(([a],[b])=>b-a)[1][1];
