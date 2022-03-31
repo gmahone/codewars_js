aa938200779f88774e3f50a80b498ff3ecbef5a4
gmahone <gmahone@gmail.com>
2022-03-31 21:39:52 +0200

    add other solutions

--- small_enough.js
+++ small_enough.js
@@ -3,3 +3,20 @@ function smallEnough(array, limit){
 }
 
 const smallEnough = (a, limit) => ![...a].map( a => a <= limit).includes(false);
+
+
+// other solutions
+
+function smallEnough(a, limit){
+  return Math.max(...a) <= limit
+}
+
+smallEnough = (a, l) => a.every(e => e <= l)
+
+const smallEnough = ($, limit) => $.every(el => el <= limit)
+
+function smallEnough(array, limit){
+    return array.filter( (value) => value > limit ).length > 0 ? false : true;
+}
+
+const smallEnough = (array, limit) => !array.some(n => n > limit);
