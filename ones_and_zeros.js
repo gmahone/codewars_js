a5e7b4b1989b00160fa964e070d05a4680352e4d
gmahone <gmahone@gmail.com>
2022-04-01 09:53:03 +0200

    add other solutions

--- ones_and_zeros.js
+++ ones_and_zeros.js
@@ -1,1 +1,15 @@
 const binaryArrayToNumber = arr => parseInt( arr.map( a => a.toString() ).join(""), 2 );
+
+// other solutions
+
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
+
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(""), 2)
+};
+
+function binaryArrayToNumber(arr) {
+  return arr.reduce( (a, b) => a << 1 | b );
+}
+
+let binaryArrayToNumber=a=>+('0b'+a.join``)
