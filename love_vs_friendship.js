01505e4d1a792294142ec69873976a6ce87f2bec
gmahone <gmahone@gmail.com>
2022-04-02 15:19:19 +0200

    add other solutions

--- love_vs_friendship.js
+++ love_vs_friendship.js
@@ -10,3 +10,31 @@ function wordsToMarks(string){
     return string.split("").map( a => charConverter(a, 0)).reduce( (acc, c) => acc + c, 0);
 }
 
+// other solutions
+
+function wordsToMarks(str)
+{
+  var sum = 0;
+  for (let i = 0; i < str.length; i++)
+    sum += str.charCodeAt(i) - 96;
+  return sum;
+}
+
+function wordsToMarks(string) {
+  return string.split('').reduce((acc, char) => {
+    return acc + char.charCodeAt() - 96;
+  }, 0);
+}
+
+function wordsToMarks(string){
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let marks = 0;
+  string.split('').forEach((char) => {
+    marks += alphabet.indexOf(char) + 1;
+  });
+  return marks;
+}
+
+const wordsToMarks = (abc) =>  [0,...abc].reduce(($, b) => $ + ' abcdefghijklmnopqrstuvwxyz'.indexOf(b))
+
+const wordsToMarks = s => [...s].map((a,i) => s.charCodeAt(i)-96).reduce((a,b) => a+b);
