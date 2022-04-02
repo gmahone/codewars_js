48e4a5d0695efc0be09cd86c26cf17c86ffd4a8f
gmahone <gmahone@gmail.com>
2022-04-02 21:32:47 +0200

    add other solutions

--- form_the_minimum.js
+++ form_the_minimum.js
@@ -1,3 +1,23 @@
 function minValue(values){
   return Number([...new Set(values)].sort( (a,b) => a - b).join(""));
 }
+
+// other solutions
+function minValue(values){
+  let arr = Array.from(new Set(values))
+  return parseInt(arr.sort().join(''))
+}
+
+function minValue(values){
+  const n = values
+    .filter((e, i, arr) => arr.indexOf(e) === i)
+    .sort()
+    .join('');
+  return Number(n);
+}
+
+function minValue(values){
+  return Number(Array.from(new Set(values)).sort().join(''))
+}
+
+const minValue = (v) => +[...new Set(v)].sort().join``
