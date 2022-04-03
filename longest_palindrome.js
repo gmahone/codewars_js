221cb2f4b8646107e7974a588fb6791e049db69a
gmahone <gmahone@gmail.com>
2022-04-03 21:48:35 +0200

    add additional single return solution

--- longest_palindrome.js
+++ longest_palindrome.js
@@ -24,3 +24,13 @@ longestPalindrome=function(s){
     }
   }
 }
+
+var longestPalindrome=function(s){
+  if (!s) return 0;
+  for (let c = s.length; c > 0; c--) {
+    for (let i = 0; i <= s.length - c; i++) {
+      var check = s.substr(i, c);
+      if (check === check.split("").reverse().join("")) return c;
+    }
+  }
+}
