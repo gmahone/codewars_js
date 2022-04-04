3b03633babb12260f0b6f5c9824e12080533712f
gmahone <gmahone@gmail.com>
2022-04-04 12:22:23 +0200

    add other solutions

--- xbonacci.js
+++ xbonacci.js
@@ -9,3 +9,48 @@ function Xbonacci(signature,n){
     }
     return signature;
 }
+
+// other solutions
+const Xbonacci = (sig, n) => {
+  let len = sig.length;
+  for (let i = len; i < n; i++) 
+    sig[i] = sig.slice(i - len).reduce((a, b) => a + b);
+  return sig.slice(0, n);
+}
+
+function Xbonacci(s, n){
+  var len = s.length, i = len - 1;
+  while (i++ < n) {
+    s[i] = s.slice(i - len).reduce(sum, 0);
+  }
+  return s.slice(0, n);
+}
+
+function sum(s, v){ return s + v }
+
+function Xbonacci(signature, n){
+  var result = signature.slice(0, n);
+  var length = signature.length;
+  for (var index = length; index < n; index++){
+    result[index] = 0;
+    for (var index2 = 1; index2 <= length; index2++){
+      result[index] += result[index - index2];
+    }
+  }
+  return result;
+}
+
+
+function Xbonacci(arr, n){
+  const result = arr.slice(0, n)
+  const length = arr.length
+  while (result.length < n) {
+    result.push(result.slice(-length).reduce((a, b) => a + b))
+  }
+  return result
+}   
+
+// recursive
+const Xbonacci = (signature, n, l = signature.length) =>
+  signature.length < n ? Xbonacci([...signature, signature.slice(signature.length - l).reduce((pre, val) => pre + val)], n, l) : signature.slice(0, n);
+
