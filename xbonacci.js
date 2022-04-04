5d898220d22c079e6ad4b3b6f71f95f19deb4965
gmahone <gmahone@gmail.com>
2022-04-04 12:19:22 +0200

    fixed bug for n smaller than init signature length

--- xbonacci.js
+++ xbonacci.js
@@ -1,4 +1,7 @@
 function Xbonacci(signature,n){
+    if(n < signature.length){
+        return signature.slice(0, n);
+    }
     let i = 0;
     while(signature.length < n){
         signature.push(signature.slice(i, signature.length).reduce( (acc,c) => acc + c));
