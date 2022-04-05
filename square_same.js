9c5a6a21b78eab3b45a20c044e4e6834e982a2b8
gmahone <gmahone@gmail.com>
2022-04-05 17:53:08 +0200

    add other solutions

--- square_same.js
+++ square_same.js
@@ -12,3 +12,27 @@ function comp(a, b){
     }
     return true;
 }
+
+// other solutions
+function comp(array1, array2) {
+  if(array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+  return array1.map(v => v * v).every((v, i) => v == array2[i]);
+}
+
+function comp(a, b) {
+  return !!a && !!b && a.map(x => x*x).sort().join() == b.sort().join();
+}
+
+const comp = (array1, array2) => 
+  Array.isArray(array1) &&
+  Array.isArray(array2) &&
+  array1.every(item => {
+    const index = array2.indexOf(Math.pow(item, 2))
+    return index > -1 ? array2.splice(index, 1) : false
+  })
+
+function comp(a, b) {
+    if (!a || !b || a.length !== b.length) return false;
+    return a.map(x => x * x).sort().toString() === b.sort().toString();
+}
