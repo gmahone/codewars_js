52247b7eaaa46d1f47778e3a4887f78d10799dd7
gmahone <gmahone@gmail.com>
2022-04-07 20:48:06 +0200

    add other solutions

--- count_smileys.js
+++ count_smileys.js
@@ -9,3 +9,18 @@ function countSmileys(arr) {
 // my solution basically trial and error until I could pass all test :/
 
 
+// other solutions
+
+// great solution for understanding character search in JS
+function countSmileys(arr) {
+  return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+}
+
+// also now with reduce
+const countSmileys = ss => ss.reduce((a, s) => a + /^[:;][-~]?[D)]$/.test(s), 0);
+
+// separation of search set
+const SMILING = /[:;]{1}[-~]?[)D]{1}/;
+const countSmileys = (faces) => faces.filter(face => SMILING.test(face)).length;
+
+// ? means matches 0 or 1 times
