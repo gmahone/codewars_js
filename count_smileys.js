75d6cd2e70172958cc7699c934730bd678a20ee8
gmahone <gmahone@gmail.com>
2022-04-07 20:44:04 +0200

    add comment

--- count_smileys.js
+++ count_smileys.js
@@ -6,3 +6,6 @@ function countSmileys(arr) {
     return arr.map( a => a.match(/^[;:]/) && a.match(/[)D]+$/) && !(a.match(/[o_]/)) && a.length<4 ? 1 : 0).reduce((acc,c) => acc + c)
   }
 }
+// my solution basically trial and error until I could pass all test :/
+
+
