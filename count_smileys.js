9af3ff2bc1bc5bd0a80417a501fd5473ec896336
gmahone <gmahone@gmail.com>
2022-04-07 20:43:26 +0200

    Create count_smileys.js

---
+++ count_smileys.js
@@ -0,0 +1,8 @@
+function countSmileys(arr) {
+  console.log(arr);
+  if(arr.length === 0){
+    return 0;
+  } else {
+    return arr.map( a => a.match(/^[;:]/) && a.match(/[)D]+$/) && !(a.match(/[o_]/)) && a.length<4 ? 1 : 0).reduce((acc,c) => acc + c)
+  }
+}
