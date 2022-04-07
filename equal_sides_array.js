076ba13c80a461be762371319361e44e1fbeeccc
gmahone <gmahone@gmail.com>
2022-04-07 16:57:51 +0200

    add other solutions

--- equal_sides_array.js
+++ equal_sides_array.js
@@ -13,3 +13,26 @@ function findEvenIndex(arr){
     }
     return result;
 }
+
+// other solutions
+
+const sum = (a, from, to) => a.slice(from, to).reduce((a, b) => a + b, 0)
+const findEvenIndex = a => a.findIndex((el, i) => sum(a, 0, i) === sum(a, i + 1));
+
+
+function findEvenIndex(arr)
+{
+  function sum(arr){
+    return arr.reduce(function(a,b){return a+b;},0);
+  }
+
+  return arr.findIndex(function(el,i,arr){
+    return sum(arr.slice(0, i)) === sum(arr.slice(i+1,arr.length));
+  });
+}
+
+
+function findEvenIndex(arr)
+{
+  return arr.findIndex((e,i,a)=> a.slice(0,i).reduce((p,c)=>p+c,0)==a.slice(i+1).reduce((p,c)=>p+c,0));
+}
