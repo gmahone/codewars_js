ff03ff905b7617b80ea4c021b71779f8870bc5e4
gmahone <gmahone@gmail.com>
2022-04-10 21:51:29 +0200

    add other solutions

--- playing_with_digits.js
+++ playing_with_digits.js
@@ -6,3 +6,17 @@ function digPow(n, p){
   }
   return result;
 }
+
+// other solutions
+
+function digPow(n, p) {
+  var x = String(n).split("").reduce((s, d, i) => s + Math.pow(d, p + i), 0)
+  return x % n ? -1 : x / n
+}
+
+function digPow(n, p){
+  var ans = (''+n).split('')
+    .map(function(d,i){return Math.pow(+d,i+p) })
+    .reduce(function(s,v){return s+v}) / n
+  return ans%1 ? -1 : ans    
+}
