72678d204ce830a54e75cfd38dc19f1cb5839598
gmahone <gmahone@gmail.com>
2022-04-10 21:49:01 +0200

    fixed bug in submitted version

--- playing_with_digits.js
+++ playing_with_digits.js
@@ -1,9 +1,8 @@
-// submitted version
 function digPow(n, p){
-    let tmp = n.toString().split("").map((a,b) => Number(a)**(p+b)).reduce((acc,c) => acc + c);
-    if(tmp % n == 0){
-        return(tmp / n);
-    } else {
-        return(-1);
-    }
+  let result = -1;
+  let powSum = n.toString().split("").map((a,b) => Number(a)**(p+b)).reduce((acc,c) => acc + c);
+  if( powSum % n === 0){
+    result = powSum / n;
+  }
+  return result;
 }
