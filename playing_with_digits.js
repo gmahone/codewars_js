3f401c97ca324eb3bc1b41356e05921175caeda4
gmahone <gmahone@gmail.com>
2022-04-10 21:47:19 +0200

    Create playing_with_digits.js

---
+++ playing_with_digits.js
@@ -0,0 +1,9 @@
+// initial version
+function digPow(n, p){
+    let tmp = n.toString().split("").map((a,b) => Number(a)**(p+b)).reduce((acc,c) => acc + c);
+    if(tmp % n == 0){
+        return(tmp / n);
+    } else {
+        return(-1);
+    }
+}
