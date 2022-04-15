f36712081720431a571712799342739d10faae7e
gmahone <gmahone@gmail.com>
2022-04-15 20:51:00 +0200

    Update code to newer sort algorithm

--- prize_draw.js
+++ prize_draw.js
@@ -18,3 +18,17 @@ let test = prizeDraw(st = "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH", we = [1, 4,
 
 // this algorithm will fail if a tie
 // needs to align with the python version which means sort alphabetically then by rank
+function prizeDraw(st, we, n){
+    let rankArray = [];
+    let result;
+    st = st.split(",");
+    for(i = 0; i < st.length; i++){
+        rankArray.push({name : st[i], 
+            rank : (st[i].length + st[i].toUpperCase().split("").map(a => a.charCodeAt()-64).reduce((acc,c)=>acc + c)) * we[i]});
+    }
+    rankArray.sort((a,b) => a.name - b.name);
+    rankArray.sort((a,b) => b.rank - a.rank);
+    result = rankArray[n-1].name
+    //console.log(result)
+    return(result)
+}
