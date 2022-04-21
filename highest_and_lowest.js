a7b59ef5b6c8ec9b90a2b40ab4bba8ace8a8daab
gmahone <gmahone@gmail.com>
2022-04-21 12:24:34 +0200

    add other solutions

--- highest_and_lowest.js
+++ highest_and_lowest.js
@@ -2,3 +2,20 @@ function highAndLow(numbers){
   const numbersSplit = numbers.split(" ").map( a => Number(a));
   return( Math.max(...numbersSplit) + " " + Math.min(...numbersSplit));
 }
+
+// add other solutions
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ').map(Number);
+  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ');
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
+
+function highAndLow(numbers) {
+  var arr = numbers.split(" ");
+  return Math.max.apply(null, arr) + ' ' + Math.min.apply(null, arr);
+}
