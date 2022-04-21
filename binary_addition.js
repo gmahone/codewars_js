f43ee162f497b754c4a7b07a397014694b341584
gmahone <gmahone@gmail.com>
2022-04-21 11:27:24 +0200

    fixed large number bug

--- binary_addition.js
+++ binary_addition.js
@@ -1,6 +1,10 @@
 function addBinary(a,b) {
   const c = a + b
-  return (c >>> 0).toString(2);
+  //return (c >>> 0).toString(2);
+  return c.toString(2);
 }
 // above solution doesn't work for all random test cases
 // presumably very large numbers
+
+
+// now fixed, didn't need the bitwise comparison
