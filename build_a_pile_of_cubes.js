a927a894b7dedfe5a116c834808303727792fb50
gmahone <gmahone@gmail.com>
2022-04-22 13:52:15 +0200

    add other solutions

--- build_a_pile_of_cubes.js
+++ build_a_pile_of_cubes.js
@@ -13,3 +13,25 @@ function findNb(m) {
   }
   return(result);
 }
+
+
+// other solutions
+
+// a count down version
+function findNb(m) {
+  var n = 0
+  while (m > 0) m -= ++n**3
+  return m ? -1 : n
+}
+
+// simplified
+function findNb(m) {
+  let n = 0;
+  let sum = 0;
+  while (sum < m) {
+    n++;
+    sum += Math.pow(n, 3);
+  }
+  return sum === m ? n : -1;
+}
+
