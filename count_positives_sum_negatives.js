7b00eb6558de63d837021f06f225c08c77f3aebe
gmahone <gmahone@gmail.com>
2022-04-22 10:24:54 +0200

    updated code for null or empty array handling

--- count_positives_sum_negatives.js
+++ count_positives_sum_negatives.js
@@ -1,4 +1,7 @@
 function countPositivesSumNegatives(input) {
+  if(input.length === 0 || input === null){
+    return([]);
+  }
   const positiveCount = input.filter(a => a > 0).length;
   const negativeSum = input.filter(a => a < 0).reduce((acc,c) => acc + c);
   return([positiveCount, negativeSum]);
