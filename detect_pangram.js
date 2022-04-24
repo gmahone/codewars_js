bd2a6eccb679e4842726e175197963ae4550eb99
gmahone <gmahone@gmail.com>
2022-04-24 21:02:27 +0200

    Create detect_pangram.js

---
+++ detect_pangram.js
@@ -0,0 +1,13 @@
+function isPangram(string){
+  const testArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+                    "p","q","r","s","t","u","v","w","x","y","z"];
+  let currLetterIndex;
+  let result = true;
+  for(let i = 0; i < testArray.length; i++){
+    currLetterIndex = string.indexOf(testArray[i]);
+    if(currLetterIndex === -1){
+      result = false;
+    }
+  }
+  return(result);
+}
