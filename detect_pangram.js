d57486c2aa81cf380770d88a973fe6d1cb69194a
gmahone <gmahone@gmail.com>
2022-04-24 21:03:30 +0200

    improved code formatting

--- detect_pangram.js
+++ detect_pangram.js
@@ -1,6 +1,5 @@
 function isPangram(string){
-  const testArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
-                    "p","q","r","s","t","u","v","w","x","y","z"];
+  const testArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   let currLetterIndex;
   let result = true;
   for(let i = 0; i < testArray.length; i++){
