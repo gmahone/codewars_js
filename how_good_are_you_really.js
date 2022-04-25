d0825495276a7065bd2507d37e719ec519639114
gmahone <gmahone@gmail.com>
2022-04-25 22:22:09 +0200

    add other solution

--- how_good_are_you_really.js
+++ how_good_are_you_really.js
@@ -2,3 +2,13 @@ function betterThanAverage(classPoints, yourPoints) {
   const classSum = classPoints.reduce((acc, c) => acc + c, yourPoints);
   return yourPoints > (classSum/(classPoints.length+1)) ? true : false;
 }
+
+
+
+// other solutions
+
+// don't need to include your score in the average
+function betterThanAverage(classPoints, yourPoints) {
+  return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
+}
+
