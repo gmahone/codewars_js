2c47beaa2016431f0c1d05062f207c58d9acc08e
gmahone <gmahone@gmail.com>
2022-04-26 21:11:45 +0200

    add comment to first solution

--- remove_the_minimum.js
+++ remove_the_minimum.js
@@ -2,3 +2,4 @@ function removeSmallest(numbers) {
   numbers.splice(numbers.indexOf(Math.min(...numbers)),1);
   return(numbers);
 }
+// this version mutates the original array and therefore fails some testing
