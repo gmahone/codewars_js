e9f45705aac78cdd5429865ea2cd76fd8d786244
gmahone <gmahone@gmail.com>
2022-04-26 21:12:18 +0200

    add non-mutating solution

--- remove_the_minimum.js
+++ remove_the_minimum.js
@@ -3,3 +3,10 @@ function removeSmallest(numbers) {
   return(numbers);
 }
 // this version mutates the original array and therefore fails some testing
+
+// this solution doesn't mutate the first array, but maps it into a second array
+function removeSmallest(numbers) {
+  let result = numbers.map(a => a)
+  result.splice(numbers.indexOf(Math.min(...numbers)),1);
+  return(result);
+}
