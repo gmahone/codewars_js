24c66f44b427a6f3181f1e33734f21661596c8e8
gmahone <gmahone@gmail.com>
2022-04-27 20:34:21 +0200

    added solution using modulus

--- find_the_next_perfect_square.js
+++ find_the_next_perfect_square.js
@@ -6,3 +6,9 @@ function findNextSquare(sq) {
 // arrow function version
 
 const findNextSquare = sq => Number.isInteger(sq**0.5) ? ((sq**0.5)+1)**2 : -1;
+
+
+// other solution with mod
+function findNextSquare(sq) {
+  return Math.sqrt(sq)%1? -1 : Math.pow(Math.sqrt(sq)+1,2);
+}
