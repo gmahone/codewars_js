0b6fc3f44f80a8872b7565aca545cd4c2b79c4f5
gmahone <gmahone@gmail.com>
2022-04-27 20:33:41 +0200

    add arrow function version

--- find_the_next_perfect_square.js
+++ find_the_next_perfect_square.js
@@ -2,3 +2,7 @@ function findNextSquare(sq) {
   return Number.isInteger(sq**0.5) ? ((sq**0.5)+1)**2 : -1;
 }
 
+
+// arrow function version
+
+const findNextSquare = sq => Number.isInteger(sq**0.5) ? ((sq**0.5)+1)**2 : -1;
