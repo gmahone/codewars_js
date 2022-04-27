64580a7b9f8f1b392a7ffaaf85616437131457a6
gmahone <gmahone@gmail.com>
2022-04-27 20:51:13 +0200

    add other solution

--- playing_banjo.js
+++ playing_banjo.js
@@ -1,3 +1,9 @@
 function areYouPlayingBanjo(name) {
   return name[0].toLowerCase() === "r" ? `${name} plays banjo` : `${name} does not play banjo` ;
 }
+
+
+// other solution
+function areYouPlayingBanjo(name) {
+  return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
