80cf591ba08633bfeb9b936fd951c84eed508db7
gmahone <gmahone@gmail.com>
2022-04-27 20:46:36 +0200

    add for loop version

--- growth_of_a_population.js
+++ growth_of_a_population.js
@@ -6,3 +6,13 @@ function nbYear(p0, percent, aug, p) {
   }
   return i;
 }
+
+
+// for loop version
+function nbYear(p0, percent, aug, p) {
+    
+  for (var years = 0; p0 < p; years++) {
+    p0 = Math.floor(p0 + p0 * percent / 100 + aug);
+  }
+  return years
+}
