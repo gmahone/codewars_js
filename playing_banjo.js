e0d09fa3bb200c52b6116c0ebcb887eac515b459
gmahone <gmahone@gmail.com>
2022-04-27 20:51:52 +0200

    add arrow function version

--- playing_banjo.js
+++ playing_banjo.js
@@ -2,6 +2,9 @@ function areYouPlayingBanjo(name) {
   return name[0].toLowerCase() === "r" ? `${name} plays banjo` : `${name} does not play banjo` ;
 }
 
+// arrow function version
+const areYouPlayingBanjo = name => name[0].toLowerCase() === "r" ? `${name} plays banjo` : `${name} does not play banjo` ;
+
 
 // other solution
 function areYouPlayingBanjo(name) {
