26068b18af01b858ef6276d448b88c81503392f4
gmahone <gmahone@gmail.com>
2022-04-28 21:43:49 +0200

    added comment to why solution doesn't work

--- your_order_please.js
+++ your_order_please.js
@@ -1,5 +1,13 @@
 function order(words){
-  let newOrder = words.split("").filter(a => a.match(/([0-9])/));
-  let result = newOrder.map(a => words.split(" ")[a-1])
+  let newOrder = words.split("").filter(a => a.match(/([0-9])/)).map(a => Number(a)-1);
+  console.log(newOrder);
+  let result = newOrder.map(a => words.split(" ")[a])
+  console.log(result);
   return(result.join(" "));
 }
+
+// the above doesn't work because it is merely taking the index in the order in which its given
+// means if its passed [1,0,2] it doesn't reorder to [0,1,2], instead takes 1 first, then 0, then 2 index of the starting array
+
+
+
