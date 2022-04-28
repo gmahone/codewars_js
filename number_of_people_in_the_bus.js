e2b27864478470edf49afb4ceb96f4619a4950da
gmahone <gmahone@gmail.com>
2022-04-28 22:04:10 +0200

    add other solutions

--- number_of_people_in_the_bus.js
+++ number_of_people_in_the_bus.js
@@ -5,3 +5,19 @@ var number = function(busStops){
 
 // arrow function version
 const number = busStops => busStops.reduce((acc, c) => acc + c[0] - c[1], 0);
+
+// other solutions
+const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
+// can directly create the array variables like above
+
+
+
+// for loop solution
+var number = function(busStops){
+  var totalPeople = 0;
+  for (var i=0; i<busStops.length; i++) {
+    totalPeople += busStops[i][0];
+    totalPeople -= busStops[i][1];
+  }
+  return totalPeople;
+}
