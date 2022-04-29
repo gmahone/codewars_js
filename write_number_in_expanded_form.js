c86c335def1191a0650d53eb42fef4f5255bc1a5
gmahone <gmahone@gmail.com>
2022-04-29 17:31:36 +0200

    add two more loop solutions

--- write_number_in_expanded_form.js
+++ write_number_in_expanded_form.js
@@ -30,4 +30,21 @@ function expandedForm(num) {
           .join(" + ")
 }
 
+var expandedForm = (num) => {
+  var arr = num.toString().split('').reverse();
+  var result = [];
+  for(var i = 0; i < arr.length; i++){
+    arr[i] == 0 ? result.push() : result.push(arr[i] + ('0'.repeat(i)))
+  }
+  return result.reverse().join(' + ')
+}
 
+function expandedForm(num) {
+  num = String(num);
+  let result = [];
+  for (var i = 0; i < num.length; i++) {
+    if (num[i] == 0) continue;
+    else result.push(num[i] + "0".repeat(num.length -i -1))
+  }
+  return result.join(" + ")
+}
