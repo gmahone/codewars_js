a1484bcf5142ce679bba03a9f999ac57644747e6
gmahone <gmahone@gmail.com>
2022-04-29 17:31:03 +0200

    add expanded function version of single chain solution

--- write_number_in_expanded_form.js
+++ write_number_in_expanded_form.js
@@ -21,3 +21,13 @@ const expandedForm = n => n.toString()
                             .reverse()
                             .join(" + ");
 
+
+function expandedForm(num) {
+  return String(num)
+          .split("")
+          .map((num, index, arr) => num + "0".repeat(arr.length - index -1 ))
+          .filter((num) => Number(num) != 0)
+          .join(" + ")
+}
+
+
