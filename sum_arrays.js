84bdf341fe46699c0b669164800fb7593a803589
gmahone <gmahone@gmail.com>
2022-05-02 22:04:56 +0200

    add standard function solution

--- sum_arrays.js
+++ sum_arrays.js
@@ -1,2 +1,8 @@
 // arrow function version
 const sum = numbers => numbers.reduce((acc,c) => acc + c, 0);
+
+
+// standard function version
+function sum(numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
+}
