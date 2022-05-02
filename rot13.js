bc20247e1749dfbe801e93c7e63c66c9d9b732c1
gmahone <gmahone@gmail.com>
2022-05-02 22:00:46 +0200

    added other solutions

--- rot13.js
+++ rot13.js
@@ -49,3 +49,65 @@ function rot13(message){
   }
   return(result);
 }
+
+
+
+// other solutions
+function rot13(message) {
+  var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+  return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
+}
+
+const rot13 = str => str.replace(/[a-z]/gi, letter => String.fromCharCode(letter.charCodeAt(0) + (letter.toLowerCase() <= 'm' ? 13: -13)));
+
+
+var codeA = 'A'.charCodeAt(0),
+    codeZ = 'Z'.charCodeAt(0),
+    codea = 'a'.charCodeAt(0),
+    codez = 'z'.charCodeAt(0);
+function rot13(message){
+  return message.split('').map(function(char) { 
+    var code = char.charCodeAt(0);
+    if(codeA <= code && code <= codeZ){
+      return String.fromCharCode(((code - codeA) + 13) % 26 + codeA);
+    } else if(codea <= code && code <= codez){
+      return String.fromCharCode(((code - codea) + 13) % 26 + codea);
+    }
+    return char;
+  }).join('');
+}
+
+
+function rot13(message) {
+  var abc = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM';
+  return message.replace(/[a-z]/gi, c => abc[abc.indexOf(c) + 13]);
+}
+
+
+
+var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXWZ";
+var cipher   = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXWZABCDEFGHIJKLM";
+
+function rot13(message){
+  return message.split('').map(function(c) {
+    var i = alphabet.indexOf(c);
+    if (i < 0) {
+      // not in alphabet, return char
+      return c;
+    }
+    
+    return cipher[i];
+  }).join('');
+}
+
+
+
+function rot13(message){
+  //your code here
+  return (message + '')
+    .replace(/[a-z]/gi, function(s) {
+      return String.fromCharCode(s.charCodeAt(0) + (s.toLowerCase() < 'n' ? 13 : -13));
+    });
+
+}
