5d44223086804ab9fbf808d29cccca6c893ca484
gmahone <gmahone@gmail.com>
2022-05-02 22:25:31 +0200

    add loop solution

--- grasshopper_summation.js
+++ grasshopper_summation.js
@@ -1,3 +1,15 @@
 var summation = function (num) {
   return new Array(num).fill(0).map((v, i) => i + 1).reduce((acc,c) => acc + c);
 }
+
+// add other solutions
+
+// loop solution
+var summation = function (num) {
+  let result = 0;
+  for (var i = 1; i <= num; i++) {
+    result += i;
+  }
+  
+  return result;
+}
