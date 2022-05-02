2627df259fc79e0b4498013df8b460d7b26e4568
gmahone <gmahone@gmail.com>
2022-05-02 22:11:13 +0200

    updated code to submitted solution

--- exes_and_ohs.js
+++ exes_and_ohs.js
@@ -1,6 +1,6 @@
-function XO(string){
-  let oNumber = string.split("").filter(a => a.toLowerCase() === "o").length;
-  let xNumber = string.split("").filter(a => a.toLowerCase() === "x").length;
+function XO(str) {
+  let oNumber = str.split("").filter(a => a.toLowerCase() === "o").length;
+  let xNumber = str.split("").filter(a => a.toLowerCase() === "x").length;
   return(oNumber == xNumber ? true : false);
 }
 
