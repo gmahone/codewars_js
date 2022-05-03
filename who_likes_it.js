e444071f9ad2667d83b80c71eb812fc7a770d435
gmahone <gmahone@gmail.com>
2022-05-03 21:46:24 +0200

    add solution

--- who_likes_it.js
+++ who_likes_it.js
@@ -1,1 +1,15 @@
-
+function likes(names) {
+  let result = " like this"
+  if(names.length == 0){
+    result = "no one likes this";
+  } else if(names.length == 1){
+    result = names[0] + " likes this";
+  } else if(names.length == 2){
+    result = names.join(" and ") + result;
+  } else if(names.length == 3){
+    result = names[0] + ", " + names[1] + " and " + names[2] + result
+  } else {
+    result = names[0] + ", " + names[1] + " and " + (names.length - 2) + " others" + result;
+  }
+  return result
+}
