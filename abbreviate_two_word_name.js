9605dcf5e0ce9737a4a4d462dcc0214af60ab631
gmahone <gmahone@gmail.com>
2022-05-03 09:00:07 +0200

    add two standard function solutions

--- abbreviate_two_word_name.js
+++ abbreviate_two_word_name.js
@@ -1,1 +1,16 @@
 const abbrevName = name => name.split(" ").map(a => a[0].toUpperCase()).join(".");
+
+
+// other solutions
+
+function abbrevName(name){
+
+  var nameArray = name.split(" ");
+  return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
+
+function abbrevName(name){
+
+    return name.split(' ').map(i => i[0].toUpperCase()).join('.')
+
+}
