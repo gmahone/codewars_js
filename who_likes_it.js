f04024d2e6ff2839fb9b047cc5ddb6bf3a088aad
gmahone <gmahone@gmail.com>
2022-05-03 21:50:41 +0200

    add regex solution

--- who_likes_it.js
+++ who_likes_it.js
@@ -38,3 +38,19 @@ function likes(names) {
     4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
   }[Math.min(4, names.length)]
 }
+
+
+function likes (names) {
+  var templates = [
+    'no one likes this',
+    '{name} likes this',
+    '{name} and {name} like this',
+    '{name}, {name} and {name} like this',
+    '{name}, {name} and {n} others like this'
+  ];
+  var idx = Math.min(names.length, 4);
+  
+  return templates[idx].replace(/{name}|{n}/g, function (val) {
+    return val === '{name}' ? names.shift() : names.length;
+  });
+}
