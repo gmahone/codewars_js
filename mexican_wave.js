64fecd71c79e2c75e04061f669fefee7b2b38342
gmahone <gmahone@gmail.com>
2022-05-04 16:26:29 +0200

    add solution using regex and forEach

--- mexican_wave.js
+++ mexican_wave.js
@@ -13,3 +13,17 @@ function wave(str){
   const result = str.split("").map((element, index, array) => element == " " ? " " : array.map((el, ind) => index == ind ? el.toUpperCase() : el).join("")).filter(a => a != " ");
   return(result);
 }
+
+
+// other solutions
+
+// using regex and forEach
+function wave(str){
+    let result = [];
+    str.split("").forEach((char, index) => {
+        if (/[a-z]/.test(char)) {
+            result.push(str.slice(0, index) + char.toUpperCase() + str.slice(index + 1));
+        }
+    });
+    return result;
+}
