f3402b3b6648953d4a5338d7e39f143128a55cbc
gmahone <gmahone@gmail.com>
2022-05-04 16:23:22 +0200

    add submitted solution

--- mexican_wave.js
+++ mexican_wave.js
@@ -5,3 +5,11 @@ console.log("hello".split("").map((element, index, array) => {array.splice(index
 
 // working solution
 console.log("hello".split("").map((element, index, array) => array.map((el, ind) => index == ind ? el.toUpperCase() : el).join("")))
+
+
+
+// solution that handles spaces " "
+function wave(str){
+  const result = str.split("").map((element, index, array) => element == " " ? " " : array.map((el, ind) => index == ind ? el.toUpperCase() : el).join("")).filter(a => a != " ");
+  return(result);
+}
