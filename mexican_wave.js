d4301b724c21d9d8af4894fc81d88b539941a913
gmahone <gmahone@gmail.com>
2022-05-04 16:27:36 +0200

    add succinct arrow function

--- mexican_wave.js
+++ mexican_wave.js
@@ -27,3 +27,7 @@ function wave(str){
     });
     return result;
 }
+
+
+// arrow function that is succinct
+var wave=w=>[...w].map((a,i)=>w.slice(0,i)+a.toUpperCase()+w.slice(i+1)).filter(a=>a!=w)
