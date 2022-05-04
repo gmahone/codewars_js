51e7dc4d6b8e95bce3a3d0c89ed1329a2a3937db
gmahone <gmahone@gmail.com>
2022-05-04 15:26:07 +0200

    add solution using Set

--- isograms.js
+++ isograms.js
@@ -1,1 +1,9 @@
 const isIsogram = str => str.toUpperCase().split("").sort().filter((a,i,arr) => a === arr[i-1]).length ? false : true;
+
+
+// other solutions
+
+// solution using Set
+function isIsogram(str){
+  return new Set(str.toUpperCase()).size == str.length;
+}
