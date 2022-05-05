2d7dbd5e904135cf1f04c6929dfb6f1ea3dcdfc3
gmahone <gmahone@gmail.com>
2022-05-05 17:10:32 +0200

    add arrow function solution

--- will_you_make_it.js
+++ will_you_make_it.js
@@ -1,3 +1,7 @@
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   return(mpg*fuelLeft >= distanceToPump ? true : false);
 };
+
+
+// arrow function version
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
