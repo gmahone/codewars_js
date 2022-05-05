2abd7fcd49b73764d1d67c824ac2e762f3925a85
gmahone <gmahone@gmail.com>
2022-05-05 17:02:17 +0200

    add arrow function version

--- remove_exclamation_marks.js
+++ remove_exclamation_marks.js
@@ -1,3 +1,6 @@
 function removeExclamationMarks(s) {
   return(s.split("").filter(a => a!="!").join(""));
 }
+
+// arrow function version
+const removeExclamationMarks = s => s.split("").filter(a => a!="!").join("");
