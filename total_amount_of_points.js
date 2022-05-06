598fd264dd57946ad42aaeb3e43925c67badcebf
gmahone <gmahone@gmail.com>
2022-05-06 21:57:09 +0200

    add normal function with loop solution

--- total_amount_of_points.js
+++ total_amount_of_points.js
@@ -1,1 +1,14 @@
-
+function points(games) {
+  let result = 0, game;
+  for(let i = 0; i < games.length; i++){
+    game = games[i].split(":").map(a => Number(a));
+    if(game[0] > game[1]){
+      result += 3;
+    } else if(game[0] === game[1]){
+      result += 1;
+    } else {
+      continue;
+    }
+  }
+  return(result);
+}
