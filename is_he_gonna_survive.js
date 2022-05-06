623add251351c320731de8671ecb04bccc801b6c
gmahone <gmahone@gmail.com>
2022-05-06 21:43:39 +0200

    add simplified arrow function solution

--- is_he_gonna_survive.js
+++ is_he_gonna_survive.js
@@ -13,3 +13,7 @@ const hero = (bullets, dragons) => bullets >= dragons*2 ? true : false;
 function hero(bullets, dragons){
   return bullets >= dragons * 2
 }
+
+
+// simplified arrow function solution
+const hero = (bullets, dragons) => dragons * 2 <= bullets;
