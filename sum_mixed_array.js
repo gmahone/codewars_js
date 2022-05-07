06ec5f328d4f566663de1f7c4259848e97a287d4
gmahone <gmahone@gmail.com>
2022-05-07 13:13:04 +0200

    add solution separating conversion and sum

--- sum_mixed_array.js
+++ sum_mixed_array.js
@@ -5,3 +5,10 @@ function sumMix(x){
 
 // arrow function version
 const sumMix = x => x.reduce((acc, c) => acc + +c, 0);
+
+
+// other solutions
+// first conversion via map, then reduce
+function sumMix(x){
+  return x.map(a => +a).reduce((a, b) => a + b);
+}
