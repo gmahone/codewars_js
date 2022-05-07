f83f253d9253674ae28904cf90d07dcb0d792fb2
gmahone <gmahone@gmail.com>
2022-05-07 13:00:40 +0200

    add arrow function version

--- sum_mixed_array.js
+++ sum_mixed_array.js
@@ -1,3 +1,7 @@
 function sumMix(x){
-  return(x.reduce((acc,c) => acc + +c, 0))
+  return(x.reduce((acc,c) => acc + +c, 0));
 }
+
+
+// arrow function version
+const sumMix = x => x.reduce((acc, c) => acc + +c, 0);
