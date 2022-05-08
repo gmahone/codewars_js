a4804af65e75f7301bc1d7dea82c2565930add6d
gmahone <gmahone@gmail.com>
2022-05-08 21:22:24 +0200

    add normal function solution version

--- reversed_sequence.js
+++ reversed_sequence.js
@@ -1,3 +1,10 @@
 const reverseSeq = n => {
   return(Array(n).fill(1).map((a,i) => a + i).reverse());
 };
+
+
+// normal function version
+function reverseSeq(n){
+  let result = Array(n).fill(1).map((a,i) => a + i).reverse();
+  return(result);
+};
