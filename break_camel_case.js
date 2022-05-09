3cd792256ad43d9740ea26ebb93aa731d55dfa90
gmahone <gmahone@gmail.com>
2022-05-09 21:26:19 +0200

    add loop solution

--- break_camel_case.js
+++ break_camel_case.js
@@ -1,1 +1,14 @@
+function solution(string) {
+  let arrayString = string.split("");
+  let result = [];
+  for(let i = 0; i < arrayString.length; i++){
+    let currString = arrayString[i];
+    console.log(currString)
+    if(currString === currString.toUpperCase()){
+      currString = " " + currString;
+    };
+    result.push(currString);
+  };
+  return(result.join(""));
+};
 
