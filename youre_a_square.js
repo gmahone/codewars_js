5bb87d7fcae7d9f1531afe1d393d11af6c15f35f
gmahone <gmahone@gmail.com>
2022-05-09 22:03:15 +0200

    add arrow function solution version

--- youre_a_square.js
+++ youre_a_square.js
@@ -1,3 +1,7 @@
 var isSquare = function(n){
   return(n === Math.floor((n**0.5)**2) ? true : false);
 }
+
+
+// arrow function version
+const isSquare = n => {n === Math.floor((n**0.5)**2) ? true : false};
