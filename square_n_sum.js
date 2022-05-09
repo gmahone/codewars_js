40cbe911ac58ec278a2f37fd3c40ebde1b5eef30
gmahone <gmahone@gmail.com>
2022-05-09 09:09:15 +0200

    add forEach solution

--- square_n_sum.js
+++ square_n_sum.js
@@ -4,3 +4,14 @@ function squareSum(numbers){
 
 
 const squareSum = numbers => numbers.reduce((acc, c) => acc + c**2, 0);
+
+
+// other solutions
+// forEach solution
+function squareSum(numbers){
+  var sum = 0;
+  numbers.forEach(function(n) {
+    sum += n * n
+  });
+  return sum;
+}
