90aba3ab4f02188fdb6c12fa53dcf1567a5cde37
gmahone <gmahone@gmail.com>
2022-05-09 22:05:45 +0200

    add modulo solution

--- youre_a_square.js
+++ youre_a_square.js
@@ -5,3 +5,9 @@ var isSquare = function(n){
 
 // arrow function version
 const isSquare = n => {n === Math.floor((n**0.5)**2) ? true : false};
+
+
+// mod. solution
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
