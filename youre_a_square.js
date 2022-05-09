2d97890a9c3631375a74731d96543e7f599cdb40
gmahone <gmahone@gmail.com>
2022-05-09 22:06:12 +0200

    add integer testing solution

--- youre_a_square.js
+++ youre_a_square.js
@@ -11,3 +11,7 @@ const isSquare = n => {n === Math.floor((n**0.5)**2) ? true : false};
 function isSquare(n) {
   return Math.sqrt(n) % 1 === 0;
 }
+
+
+// integer testing solution
+const isSquare = n => Number.isInteger(Math.sqrt(n));
