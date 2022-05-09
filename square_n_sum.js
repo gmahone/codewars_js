0d38cb4f41626cfbec757c696c87e14cc430849a
gmahone <gmahone@gmail.com>
2022-05-09 09:07:34 +0200

    add arrow function solution version

--- square_n_sum.js
+++ square_n_sum.js
@@ -1,3 +1,6 @@
 function squareSum(numbers){
-  return(numbers.reduce((acc, c) => acc + c**2, 0))
+  return(numbers.reduce((acc, c) => acc + c**2, 0));
 }
+
+
+const squareSum = numbers => numbers.reduce((acc, c) => acc + c**2, 0);
