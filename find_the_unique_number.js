b57afa769421119f98c4859437105bc6ef1098df
gmahone <gmahone@gmail.com>
2022-05-10 21:45:50 +0200

    add solution using index comparison

--- find_the_unique_number.js
+++ find_the_unique_number.js
@@ -11,3 +11,9 @@ function findUniq(arr) {
   arr.sort((a,b)=>a-b);
   return arr[0]==arr[1]?arr.pop():arr[0]
 }
+
+
+// solution using index comparison
+function findUniq(arr) {
+  return arr.find(n => arr.indexOf(n) === arr.lastIndexOf(n));
+}
