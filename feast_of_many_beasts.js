d55ba388b55dc2030e9f0f3aca626358359b0427
gmahone <gmahone@gmail.com>
2022-05-10 22:12:38 +0200

    add a simpler normal function solution version

--- feast_of_many_beasts.js
+++ feast_of_many_beasts.js
@@ -5,3 +5,11 @@ function feast(beast, dish) {
 
 // arrow function version
 const feast = (beast, dish) => beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1] ? true : false;
+
+
+// other solutions
+
+// simpler solution
+function feast(beast, dish) {
+  return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+}
