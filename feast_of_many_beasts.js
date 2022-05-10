95b150ad914e8430f36f2e48b6d9062ba7da21df
gmahone <gmahone@gmail.com>
2022-05-10 22:10:32 +0200

    add arrow function solution

--- feast_of_many_beasts.js
+++ feast_of_many_beasts.js
@@ -1,3 +1,7 @@
 function feast(beast, dish) {
   return(beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1] ? true : false);
 };
+
+
+// arrow function version
+const feast = (beast, dish) => beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1] ? true : false;
