beebf2949e99b4c9c4ec94d398d55f245669199e
gmahone <gmahone@gmail.com>
2022-05-10 21:43:34 +0200

    add sort solution using pop

--- find_the_unique_number.js
+++ find_the_unique_number.js
@@ -2,3 +2,12 @@ function findUniq(arr) {
   let sortArr = arr.sort();
   return(sortArr[0]===sortArr[1] ? sortArr[arr.length - 1] : sortArr[0]);
 }
+
+
+// other solutions
+
+// similar sort solution but using pop()
+function findUniq(arr) {
+  arr.sort((a,b)=>a-b);
+  return arr[0]==arr[1]?arr.pop():arr[0]
+}
