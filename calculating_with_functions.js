e7382db03cb187a28e7af15ddc17ba4528836aac
gmahone <gmahone@gmail.com>
2022-05-11 13:44:08 +0200

    add solution - digit function logic is abstracted

--- calculating_with_functions.js
+++ calculating_with_functions.js
@@ -130,3 +130,26 @@ function plus( b )      { return function( a ) { return a + b; }; };
 function minus( b )     { return function( a ) { return a - b; }; };
 function times( b )     { return function( a ) { return a * b; }; };
 function dividedBy( b ) { return function( a ) { return a / b; }; };
+
+
+// digit logic abstracted into own function
+var n = function(digit) {
+  return function(op) {
+    return op ? op(digit) : digit;
+  }
+};
+var zero = n(0);
+var one = n(1);
+var two = n(2);
+var three = n(3);
+var four = n(4);
+var five = n(5);
+var six = n(6);
+var seven = n(7);
+var eight = n(8);
+var nine = n(9);
+
+function plus(r) { return function(l) { return l + r; }; }
+function minus(r) { return function(l) { return l - r; }; }
+function times(r) { return function(l) { return l * r; }; }
+function dividedBy(r) { return function(l) { return l / r; }; }
