34ce93d79180562196582481067c4c7b2299901a
gmahone <gmahone@gmail.com>
2022-05-12 16:19:13 +0200

    add single+ loop with hash table

--- two_sum.js
+++ two_sum.js
@@ -21,3 +21,15 @@ function twoSum(numbers, target) {
         }
     }
 }
+
+
+// single+ loop with hash table
+function twoSum(numbers, target) {
+  var tmp, hash = {};
+  numbers.forEach(function(a, i){ hash[a] = i; })
+  
+  for (var i = 0; i < numbers.length; i++){
+    tmp = target - numbers[i];
+    if (typeof hash[tmp] !== 'undefined') return [i, hash[tmp]]
+  }
+}
