67bac43faab1a56ac633daa6f9dbfaf558a54066
gmahone <gmahone@gmail.com>
2022-05-12 15:21:39 +0200

    add normal function solution

--- two_sum.js
+++ two_sum.js
@@ -1,1 +1,11 @@
-
+function twoSum(numbers, target) {
+  let newMap = {};
+  for(let i = 0; i < numbers.length; i++){
+    let targetDiff = target - numbers[i];
+    if(newMap.hasOwnProperty(targetDiff)){
+      return([newMap[targetDiff], i]);
+    }
+    newMap[numbers[i]] = i;
+  }
+  return([]);
+}
