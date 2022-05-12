f7ead02090d84db09c1ace0c976c980d4c91be0f
gmahone <gmahone@gmail.com>
2022-05-12 20:57:48 +0200

    add shorter solution using slice

--- jaden_casing_strings.js
+++ jaden_casing_strings.js
@@ -8,3 +8,13 @@ String.prototype.toJadenCase = function () {
   }
   return(result.join(" "));
 };
+
+
+// add other solutions
+
+// nice solution using slice
+String.prototype.toJadenCase = function () { 
+  return this.split(" ").map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(" ");
+}
