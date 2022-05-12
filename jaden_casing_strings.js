8e9c2f94c1525c5fc992abae75e60f36677650b5
gmahone <gmahone@gmail.com>
2022-05-12 20:56:35 +0200

    add solution

--- jaden_casing_strings.js
+++ jaden_casing_strings.js
@@ -1,1 +1,10 @@
-
+String.prototype.toJadenCase = function () {
+  let splitStr = this.split(" ");
+  let result = [];
+  for(let i = 0; i < splitStr.length; i++){
+    let tmpWord = splitStr[i].split("");
+    tmpWord[0] = tmpWord[0].toUpperCase();
+    result.push(tmpWord.join(""));
+  }
+  return(result.join(" "));
+};
