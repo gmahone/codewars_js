713733faef69fee8939e09f1789acb0fa5a1d137
gmahone <gmahone@gmail.com>
2022-05-12 16:15:37 +0200

    add double loop slow solution

--- two_sum.js
+++ two_sum.js
@@ -9,3 +9,15 @@ function twoSum(numbers, target) {
   }
   return([]);
 }
+
+
+// other solutions
+
+// lazy double loop solution
+function twoSum(numbers, target) {
+    for (var i = 0; i < numbers.length-1; i++) {
+        for (var j = i+1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) return [i, j];
+        }
+    }
+}
