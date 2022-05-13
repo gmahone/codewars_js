549566a8514114a100beb50fb290af36c68d51c4
gmahone <gmahone@gmail.com>
2022-05-13 22:00:53 +0200

    add arrow function using an object

--- traffic_light.js
+++ traffic_light.js
@@ -1,3 +1,13 @@
 function updateLight(current) {
   return(current == "green" ? "yellow" : current == "yellow" ? "red" : "green");
 }
+
+
+// other solutions
+
+// arrow function via object
+const updateLight = current => ({
+  green: 'yellow',
+  yellow: 'red',
+  red: 'green',
+})[current]
