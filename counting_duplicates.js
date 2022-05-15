3307d1560ded6e93ea13c5258a5270ff2660f500
gmahone <gmahone@gmail.com>
2022-05-15 22:12:25 +0200

    start PREP

--- counting_duplicates.js
+++ counting_duplicates.js
@@ -1,3 +1,8 @@
+// Parameters - a text string containing letters and numbers
+// Return - should return an integer for how many letters and numbers have duplicates
+// Exceptions - the input will be only alphanumeric, but may have upper and lower case letters and this should be controlled
+// Pseudocode:
+
 function duplicateCount(text){
   //...
 }
