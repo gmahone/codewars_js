7cd98f1315d6e6d8255b2fb93880257b2e13a1c1
gmahone <gmahone@gmail.com>
2022-05-15 22:23:03 +0200

    initial solution

--- counting_duplicates.js
+++ counting_duplicates.js
@@ -2,7 +2,24 @@
 // Return - should return an integer for how many letters and numbers have duplicates
 // Exceptions - the input will be only alphanumeric, but may have upper and lower case letters and this should be controlled
 // Pseudocode:
-
+//   - Convert all to upper or lower case ( I choose upper)
+//   - Split input into array
+//   - Sort array
+//   - Loop through array
+//   - Take value at current index
+//   - Compare current value index with .lastIndexOf
+//   - If not the same, add one to duplicate count and set index value to .lastIndexOf value + 1
+//   - return duplicate count
 function duplicateCount(text){
-  //...
+  let textArray = text.toUpperCase().split("").sort();
+  let duplicateCount;
+  for(let i = 0; i < textArray.length; i++){
+    let currentValue = textArray[i];
+    let lastIndex = textArray.lastIndexOf(currentValue);
+    if(i !== lastIndex){
+      duplicateCount++;
+      i = lastIndex + 1;
+    }
+  }
+  return(duplicateCount);
 }
