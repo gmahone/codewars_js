18d1c44a02fcbfe4b5574e1473ed78af57607611
gmahone <gmahone@gmail.com>
2022-05-16 13:56:02 +0200

    add working example

--- find_the_stray_number.js
+++ find_the_stray_number.js
@@ -4,3 +4,6 @@
 // if first index != last index, then the item is duplicated.
 // if the first index == the last index, then the item is not duplicated.
 
+// working example
+let test1 = [1, 1, 2, 1, 1, 1, 1];
+test1.filter((a,i,arr) => arr.indexOf(a) === arr.lastIndexOf(a));
