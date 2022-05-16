3947629c96ca9806fc6cea089031db41194639ca
gmahone <gmahone@gmail.com>
2022-05-16 12:34:45 +0200

    add comments for prep

--- find_the_stray_number.js
+++ find_the_stray_number.js
@@ -1,1 +1,6 @@
+// input: Array of numbers
+// output: Number that is not duplicated
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+// if first index != last index, then the item is duplicated.
+// if the first index == the last index, then the item is not duplicated.
 
