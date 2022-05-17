c502318be3c6065e94b20d5420b3d262b50f8918
gmahone <gmahone@gmail.com>
2022-05-17 08:32:59 +0200

    add solution using while

--- persistant_bugger.js
+++ persistant_bugger.js
@@ -1,1 +1,11 @@
-
+function persistence(num) {
+  let i = 0;
+  while(true){
+     if(String(num).length === 1){
+       return(i);
+     }
+     num = String(num).split("").reduce((acc,c) => Number(acc) * c);
+     i++;
+  }
+  return(null);
+}
