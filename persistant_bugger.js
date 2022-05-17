23170b8f1ca31357ca76f191984ff61227904b01
gmahone <gmahone@gmail.com>
2022-05-17 08:34:22 +0200

    add clearer while solution

--- persistant_bugger.js
+++ persistant_bugger.js
@@ -9,3 +9,17 @@ function persistence(num) {
   }
   return(null);
 }
+
+// other solutions
+
+// clearer solution
+function persistence(num) {
+   let times = 0;
+   num = num.toString();
+   
+   while (num.length > 1) {
+     times++;
+     num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+   }
+   return(times);
+}
