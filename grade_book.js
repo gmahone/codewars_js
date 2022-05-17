5eb21ae5b47bea234166cbcf52e6c00d7c605971
gmahone <gmahone@gmail.com>
2022-05-17 20:54:40 +0200

    add solution

--- grade_book.js
+++ grade_book.js
@@ -1,1 +1,17 @@
-
+function getGrade (s1, s2, s3) {
+  let average = (s1+s2+s3)/3;
+  let result = "F";
+  if(average >= 60){
+    result = "D";
+  }
+  if(average >= 70){
+    result = "C";
+  }
+  if(average >= 80){
+    result = "B";
+  }
+  if(average >= 90){
+    result = "A";
+  }
+  return(result);
+}
