6c93e6d3a58a9d0419969540b7953b481ccf3b3b
gmahone <gmahone@gmail.com>
2022-05-17 08:39:10 +0200

    add recursive solution

--- persistant_bugger.js
+++ persistant_bugger.js
@@ -23,3 +23,11 @@ function persistence(num) {
    }
    return(times);
 }
+
+
+// recursive function
+const persistence = num => {
+  return `${num}`.length > 1 
+    ? 1 + persistence(`${num}`.split('').reduce((a, b) => a * +b)) 
+    : 0;
+}
