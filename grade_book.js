0c0c4aa66a771c07eae85f9618492b7623a8b04b
gmahone <gmahone@gmail.com>
2022-05-17 20:56:34 +0200

    add else if solution

--- grade_book.js
+++ grade_book.js
@@ -15,3 +15,16 @@ function getGrade (s1, s2, s3) {
   }
   return(result);
 }
+
+
+// other solutions
+
+// using else ifs
+function getGrade (s1, s2, s3) {
+  let avg = (s1+s2+s3)/3;
+  if (avg < 60)  return "F";
+    else if (avg < 70) return "D";
+    else if (avg < 80) return "C";
+    else if (avg < 90) return "B";
+    else return "A";
+}
