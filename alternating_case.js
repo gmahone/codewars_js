8502c1499c441e21fa235d40e3044fd3367fded7
gmahone <gmahone@gmail.com>
2022-05-17 21:26:00 +0200

    add solution with split arrow functions

--- alternating_case.js
+++ alternating_case.js
@@ -15,3 +15,12 @@ String.prototype.toAlternatingCase = function () {
 String.prototype.toAlternatingCase = function () {
     return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
 }
+
+
+// split into arrow functions
+const isLowerCase = (char) => char.toLowerCase() === char;
+const swapCase = (char) => isLowerCase(char) ? char.toUpperCase() : char.toLowerCase();
+
+String.prototype.toAlternatingCase = function() {
+  return [...this].map(swapCase).join('');
+};
