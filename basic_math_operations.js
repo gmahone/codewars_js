71f367c24aae3312878c12a7b24c0a70b066986d
gmahone <gmahone@gmail.com>
2022-05-18 21:03:42 +0200

    add solution using switch cases

--- basic_math_operations.js
+++ basic_math_operations.js
@@ -1,1 +1,12 @@
-
+function applyOperations(operation, a, b){
+  switch(operation){
+    case "+":
+      return(a+b);
+    case "-":
+      return(a-b);
+    case "*":
+      return(a*b);
+    case "/":
+      return(a/b);
+  }
+}
