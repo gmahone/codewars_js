d879807a464b77343d7c9ca11881fdfab4add2c5
gmahone <gmahone@gmail.com>
2022-05-19 11:10:52 +0200

    add first solution

--- highest_scoring_word.js
+++ highest_scoring_word.js
@@ -1,7 +1,12 @@
 function high(x){
   let words = x.split(" ");
-  let score = {};
+  let currentMax = 0;
+  let result;
   for(let i = 0; i < words.length; i++){
-    
+    let wordScore = words[i].split("").map(a => a.charCodeAt() - 96);
+    if(wordScore > currentMax){
+      result = words[i];
+    }
   }
+  return(result);
 }
