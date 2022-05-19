28bf4a0140bf4c42c4942882b1dcffaebb27ed93
gmahone <gmahone@gmail.com>
2022-05-19 11:11:10 +0200

    add reduce to first solution

--- highest_scoring_word.js
+++ highest_scoring_word.js
@@ -3,7 +3,7 @@ function high(x){
   let currentMax = 0;
   let result;
   for(let i = 0; i < words.length; i++){
-    let wordScore = words[i].split("").map(a => a.charCodeAt() - 96);
+    let wordScore = words[i].split("").map(a => a.charCodeAt() - 96).reduce((acc, c) => acc + c);
     if(wordScore > currentMax){
       result = words[i];
     }
