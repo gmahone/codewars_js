97cf754b78a6d1dcf841fbbb2902bece4be5772a
gmahone <gmahone@gmail.com>
2022-05-19 11:14:13 +0200

    add concise solution with no looping

--- highest_scoring_word.js
+++ highest_scoring_word.js
@@ -12,3 +12,12 @@ function high(x){
   }
   return(result);
 }
+
+
+// other solutions
+
+// very concise solution
+function high(s){
+  let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
+  return s.split(' ')[as.indexOf(Math.max(...as))];
+}
