dd1f95a27518241009aafa6bda3cf66f75eb1c73
gmahone <gmahone@gmail.com>
2022-05-19 11:15:48 +0200

    add solution using ternary in reduce

--- highest_scoring_word.js
+++ highest_scoring_word.js
@@ -21,3 +21,15 @@ function high(s){
   let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
   return s.split(' ')[as.indexOf(Math.max(...as))];
 }
+
+
+// multifunction solution
+function high(x){
+    return x.split(' ').reduce((accum, current)=>{
+        return score(current) > score(accum)? current:accum;  
+    })
+}
+
+function score(word){
+    return word.split('').reduce((accum,current)=>{return accum+(current.charCodeAt()-96)},0)
+}
