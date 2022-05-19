f4ef0ae3c2471f19bab6a130f39d8aef7e388a6c
gmahone <gmahone@gmail.com>
2022-05-19 21:43:25 +0200

    add solution using typeof number

--- list_filtering.js
+++ list_filtering.js
@@ -1,3 +1,12 @@
 function filter_list(l) {
   return(l.filter(a => Number.isInteger(a)));
 }
+
+
+// other solutions
+
+// solution using typeof number
+function filter_list(l) {
+  return l.filter(function(v) {return typeof v == 'number'})
+}
+
