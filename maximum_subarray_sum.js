7cae136bc1e2b99ac5afe6e0de70c2e340e0eac2
gmahone <gmahone@gmail.com>
2022-05-20 22:16:31 +0200

    add solution using only reduce

--- maximum_subarray_sum.js
+++ maximum_subarray_sum.js
@@ -56,3 +56,12 @@ var maxSequence = function(arr){
   }
   return ans;
 }
+
+// solution with only reduce loop
+var maxSequence = function(arr){
+    var currentSum = 0;
+    return arr.reduce(function(maxSum, number){
+        currentSum = Math.max(currentSum+number, 0);
+        return Math.max(currentSum, maxSum);
+    }, 0);
+}
