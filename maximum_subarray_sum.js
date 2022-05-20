75b701e0c09d75fb16d72177d89f792d0a3de958
gmahone <gmahone@gmail.com>
2022-05-20 21:45:04 +0200

    add an example

--- maximum_subarray_sum.js
+++ maximum_subarray_sum.js
@@ -1,2 +1,4 @@
 // input parameters - an array of integers, can be positive or negative
 // should return the maximal sum of a subarray of the input array
+// example: maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+//       -> should return 6: [4, -1, 2, 1]
