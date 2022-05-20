417755246b5c7603b0c3d10bbcffe18a0be3a9ce
gmahone <gmahone@gmail.com>
2022-05-20 22:15:36 +0200

    add solution using single loop

--- maximum_subarray_sum.js
+++ maximum_subarray_sum.js
@@ -43,3 +43,16 @@ var maxSequence = function(arr){
   }
   return(result);
 }
+
+// other solutions
+
+// solution with sums and mins
+var maxSequence = function(arr){
+  var min = 0, ans = 0, i, sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
+}
