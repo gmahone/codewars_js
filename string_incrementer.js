4d09256977411cc00dab2a8d77f1ccf01aa87e0c
gmahone <gmahone@gmail.com>
2022-05-21 21:26:28 +0200

    add output info

--- string_incrementer.js
+++ string_incrementer.js
@@ -1,3 +1,6 @@
 // input will be a string that contains numbers (could have padded zeros) 
-
+// the input string should be returned incremented by 1
+//  - the number at the right end should be increased by 1
+//  - if padded by zeros, this should be the same in output
+//  - if there is no number in the input, a 1 should be appended
 
