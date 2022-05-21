c98db901bedc64f2452cc1136a4c1f3d2f3517e8
gmahone <gmahone@gmail.com>
2022-05-21 21:28:27 +0200

    add examples

--- string_incrementer.js
+++ string_incrementer.js
@@ -3,4 +3,8 @@
 //  - the number at the right end should be increased by 1
 //  - if padded by zeros, this should be the same in output
 //  - if there is no number in the input, a 1 should be appended
+// examples:
+//  - foo -> foo1
+//  - foobar23 -> foobar24
+//  - foo0042 -> foo0043
 
