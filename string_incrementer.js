df0235c63c9f9b3e6d0b0d091e59c56973514d69
gmahone <gmahone@gmail.com>
2022-05-21 22:06:52 +0200

    add initial solution

--- string_incrementer.js
+++ string_incrementer.js
@@ -19,3 +19,29 @@
 //     return string + (number as number + 1)
 //    else string number is padded
 //     return string + (padded number + 1)
+
+function incrementer(str){
+  let result, currCharCode, subString, subNumber;
+  let lastStringIndex = str.length - 1
+  for(let i = 0; i < str.length; i++){
+    currCharCode = str[i].charCodeAt();
+    if(currCharCode >= 48 && currCharCode <= 57){
+      lastStringIndex = i - 1;
+      break;
+    }
+  }
+  if(lastStringIndex === (str.length - 1)){
+    result = str + 1;
+  } else {
+    subString = str.substring(0,lastStringIndex + 1);
+    subNumber = str.substring(lastStringIndex + 1);
+    if(subNumber.length === String(Number(subNumber)).length){
+      result = subString + String(Number(subNumber) + 1);
+    } else {
+      result = subString + String(Number(subNumber) + 1).padStart(subNumber.length, "0");
+    }
+  }
+  return result;
+}
+  
+
