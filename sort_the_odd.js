5704caf671783cd42e3b2a575828627d1ca75ecc
gmahone <gmahone@gmail.com>
2022-05-23 17:01:23 +0200

    add the parameters

--- sort_the_odd.js
+++ sort_the_odd.js
@@ -1,1 +1,4 @@
-
+// the input is an array of numbers
+//  - the array may contain both even and odd numbers
+//  - odd numbers should be ordered ascending
+//  - even numbers should be left in place
