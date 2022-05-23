1d8c0d751beb2f52c5f14aa5da30942b70b3b8d3
gmahone <gmahone@gmail.com>
2022-05-23 21:48:11 +0200

    add pseudocode

--- sort_the_odd.js
+++ sort_the_odd.js
@@ -7,3 +7,15 @@
 //  - [7, 1]  ->  [1, 7]
 //  - [5, 8, 6, 3, 4]  ->  [3, 8, 6, 5, 4]
 //  - [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+// pseudocode
+//  while TRUE
+//   loop through array
+//   if odd
+//    test if current is smaller than previous
+//    if true
+//     swap current with previous
+//     restart loop
+//   if reaches the end with no swap
+//    break while
+//  return resorted array
