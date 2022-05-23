5ce20f3fc0405bbe092b180a8cec0a1e57e3b7d2
gmahone <gmahone@gmail.com>
2022-05-23 17:02:55 +0200

    add examples

--- sort_the_odd.js
+++ sort_the_odd.js
@@ -3,3 +3,7 @@
 //  - odd numbers should be ordered ascending
 //  - even numbers should be left in place
 // an array with ordered odds and in place evens should then be returned
+// examples: 
+//  - [7, 1]  ->  [1, 7]
+//  - [5, 8, 6, 3, 4]  ->  [3, 8, 6, 5, 4]
+//  - [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
