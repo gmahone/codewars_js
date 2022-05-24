f1b73ad3ebe2c4d98dde2e6976d89ea4815b7fef
gmahone <gmahone@gmail.com>
2022-05-24 22:28:43 +0200

    add pseudocode

--- give_me_a_diamond.js
+++ give_me_a_diamond.js
@@ -7,3 +7,9 @@
 // *****
 //  ***
 //   *
+// pseudocode
+//  input integer (n) indicates also the number of lines
+//  loop i from 1...n
+//   determine space padding with max(i, (n-1)/i) - min(i, (n-1)/i)
+//   determine the number of asterisks with ((n-1)/2) - n % i)
+//   draw the current line
