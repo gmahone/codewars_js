577f3fd32bed8a1590d79ac54f228519d9b00285
gmahone <gmahone@gmail.com>
2022-05-24 21:36:22 +0200

    add a solution using parseInt

--- descending_order.js
+++ descending_order.js
@@ -1,3 +1,9 @@
 function descendingOrder(n){
   return Number(String(n).split("").sort((a,b) => b - a).join(""));
 }
+
+
+// using parseInt instead of Number
+function descendingOrder(n){
+  return parseInt(String(n).split('').sort().reverse().join(''))
+}
