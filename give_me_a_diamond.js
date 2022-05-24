2f00596a54c482ab2b59795feda09ab9f544c17e
gmahone <gmahone@gmail.com>
2022-05-24 22:45:02 +0200

    add unfinished solution

--- give_me_a_diamond.js
+++ give_me_a_diamond.js
@@ -13,3 +13,15 @@
 //   determine space padding with max(i, (n-1)/i) - min(i, (n-1)/i)
 //   determine the number of asterisks with ((n-1)/2) - n % i)
 //   draw the current line
+
+function drawDiamond(n){
+  let spacePadding;
+  let asteriskNumber;
+  let resultDiamond;
+  for(let i = 0; i < (n+1); i++){
+    spacePadding = Math.max(i, (n-1)/i) - Math.min(i, (n-1)/i);
+    asteriskNumber = (((n-1)/2) - n % i);
+    resultDiamond += " ".repeat(spacePadding) + "*".repeat(asteriskNumber) + "\n"
+  }
+  return(resultDiamond);
+}
