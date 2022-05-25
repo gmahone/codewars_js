6ad1518796547a232288828a0cefa8d53de47136
gmahone <gmahone@gmail.com>
2022-05-25 09:42:44 +0200

    formatted submitted solution

--- give_me_a_diamond.js
+++ give_me_a_diamond.js
@@ -19,8 +19,8 @@ function drawDiamond(n){
   let asteriskNumber;
   let resultDiamond = "";
   for(let i = 1; i < (n+1); i++){
-    //spacePadding = Math.max(i-1, (n-1)/2) - Math.min(i-1, (n-1)/2);
-    spacePadding = (((n-1)/2) - n % i);
+    spacePadding = Math.max(i-1, (n-1)/2) - Math.min(i-1, (n-1)/2);
+    //spacePadding = (((n-1)/2) - n % i);
     asteriskNumber = n - spacePadding*2;
     //asteriskNumber = (n % i);
     console.log(spacePadding, asteriskNumber);
@@ -38,11 +38,8 @@ function diamond(n){
   let asteriskNumber;
   let diam = "";
   for(let i = 1; i < (n+1); i++){
-    //spacePadding = Math.max(i-1, (n-1)/2) - Math.min(i-1, (n-1)/2);
-    spacePadding = (((n-1)/2) - n % i);
+    spacePadding = Math.max(i-1, (n-1)/2) - Math.min(i-1, (n-1)/2);
     asteriskNumber = n - spacePadding*2;
-    //asteriskNumber = (n % i);
-    console.log(spacePadding, asteriskNumber);
     diam += " ".repeat(spacePadding) + "*".repeat(asteriskNumber) + "\n";
   }
   return diam;
