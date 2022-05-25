cca3d75537d640abf54e7a3f2ab9eadd650250a3
gmahone <gmahone@gmail.com>
2022-05-25 09:46:32 +0200

    add different formulation solution

--- give_me_a_diamond.js
+++ give_me_a_diamond.js
@@ -44,3 +44,18 @@ function diamond(n){
   }
   return diam;
 }
+
+// other solutions
+
+// different formulation with loop
+function diamond (n) {
+  if (n <= 0 || n % 2 === 0) return null
+  str = ''
+  for (let i = 0; i < n; i++) { 
+    let len = Math.abs((n-2*i-1)/2)
+    str += ' '.repeat(len)
+    str += '*'.repeat(n-2*len)
+    str += '\n'
+  }
+  return str
+}
