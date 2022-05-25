6dd2bb40c332dff5912f3be11595790868fe522b
gmahone <gmahone@gmail.com>
2022-05-25 09:38:31 +0200

    add submitted solution

--- give_me_a_diamond.js
+++ give_me_a_diamond.js
@@ -28,3 +28,22 @@ function drawDiamond(n){
   }
   return(resultDiamond);
 }
+
+// submitted solution
+function diamond(n){
+  if(!(n & 0x1) || n <= 0){
+    return null
+  }
+  let spacePadding;
+  let asteriskNumber;
+  let diam = "";
+  for(let i = 1; i < (n+1); i++){
+    //spacePadding = Math.max(i-1, (n-1)/2) - Math.min(i-1, (n-1)/2);
+    spacePadding = (((n-1)/2) - n % i);
+    asteriskNumber = n - spacePadding*2;
+    //asteriskNumber = (n % i);
+    console.log(spacePadding, asteriskNumber);
+    diam += " ".repeat(spacePadding) + "*".repeat(asteriskNumber) + "\n";
+  }
+  return diam;
+}
