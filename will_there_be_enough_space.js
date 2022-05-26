73eb20efb4ad68dfd14670c3992f15f47917368b
gmahone <gmahone@gmail.com>
2022-05-26 21:35:52 +0200

    add arrow function version

--- will_there_be_enough_space.js
+++ will_there_be_enough_space.js
@@ -2,3 +2,6 @@ function enough(cap, on, wait) {
   let result = on + wait - cap;
   return result > 0 ? result : 0;
 }
+
+// arrow function version
+const enough = (cap, on, wait) => (on + wait - cap) > 0 ? (on + wait - cap) : 0;
