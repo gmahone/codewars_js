d8a08c0cf1f8bd8e7e28d6eecff4435578e43477
gmahone <gmahone@gmail.com>
2022-05-26 21:47:49 +0200

    add solution using array method every

--- can_we_divide_it.js
+++ can_we_divide_it.js
@@ -4,3 +4,9 @@ function isDivideBy(number, a, b) {
 
 // arrow function version
 const isDivideBy = (number, a, b) => number % a === 0 && number % b === 0;
+
+
+// add solution using .every
+function isDivideBy(number, a, b) {
+  return [a, b].every(i => number % i === 0)
+}
