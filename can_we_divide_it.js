784fe4a0f5cb7d1f13e128a1237250e712dac622
gmahone <gmahone@gmail.com>
2022-05-26 21:47:02 +0200

    add arrow function version

--- can_we_divide_it.js
+++ can_we_divide_it.js
@@ -1,3 +1,6 @@
 function isDivideBy(number, a, b) {
   return number % a === 0 && number % b === 0;
 }
+
+// arrow function version
+const isDivideBy = (number, a, b) => number % a === 0 && number % b === 0;
