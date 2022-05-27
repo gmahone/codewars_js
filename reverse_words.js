9e7b6479ce2469b2a4e4dcf13156805972b1d25f
gmahone <gmahone@gmail.com>
2022-05-27 22:05:32 +0200

    add pseudocode for new solution

--- reverse_words.js
+++ reverse_words.js
@@ -11,3 +11,17 @@ function reverseWords(str) {
 }
 // the solution above is fine without a mirrored pair but fails on "stressed desserts"
 // need to find a new solution
+//  a solution using charCodeAt for the space character code (32)
+// pseudocode:
+//  loop over length of string
+//   check for space
+//   check for within word
+//   if no space and no within word
+//    set within word as true
+//    save index value for word beginning
+//   else if space and is within word
+//    save index value - 1 for word end
+//    splice array and reverse to result string
+//   else
+//    next
+//  return result string
