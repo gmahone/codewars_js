437d74dbd7dcfa6906836f42ea67fdec70ccf89f
gmahone <gmahone@gmail.com>
2022-05-27 21:33:54 +0200

    add placeholder function

--- reverse_words.js
+++ reverse_words.js
@@ -1,3 +1,7 @@
 // possible solution without regex
 // string.split(" ").trim() then loop over, and .replace array item with reversed item
 // using regex, can avoid trim with "this is a  test".split(/\s+/)
+
+function reverseWords(str) {
+  // Go for it
+}
