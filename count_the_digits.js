3ac117d0106bffe7084f092dd89a16cfcfd5c4ae
gmahone <gmahone@gmail.com>
2022-05-29 21:08:52 +0200

    add preliminary solution

--- count_the_digits.js
+++ count_the_digits.js
@@ -1,3 +1,17 @@
+// pseudocode:
+//  initialize count of d
+//  loop from 0..n
+//   square i
+//   convert to string, split, filter by equal to d, length, add to count of d
+//  end loop
+//  return count of d
+
 function nbDig(n, d) {
-    // your code
+    let dCount = 0;
+    for(let i = 0; i < (n + 1); i++){
+        let temp = String(i * i).split("").filter(a => a === String(d)).length;
+        console.log(temp);
+        dCount += temp;
+    }
+    return dCount;
 }
