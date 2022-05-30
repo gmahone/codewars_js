05c1f802c860096bd8bdcb101052fe4fda6005e4
gmahone <gmahone@gmail.com>
2022-05-30 08:30:03 +0200

    added solution version without replaceAll

--- dna_to_rna_conversion.js
+++ dna_to_rna_conversion.js
@@ -3,3 +3,7 @@ function DNAtoRNA(dna) {
 }
 
 const DNAtoRNA = dna => dna.replaceAll("T", "U");
+
+//replaceAll not available for nodeJS v14.x
+// so a little longer solution
+const DNAtoRNA = dna => dna.split("").map(a => a.replace("T", "U")).join("");
