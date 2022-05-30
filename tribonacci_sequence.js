e5da14f57047bb4c75018ea4e3ffad110614cfde
gmahone <gmahone@gmail.com>
2022-05-30 22:09:36 +0200

    add solution with fixed index looping

--- tribonacci_sequence.js
+++ tribonacci_sequence.js
@@ -6,3 +6,13 @@ function tribonacci(signature,n){
   }
   return signature.slice(0,n);
 }
+
+// other solutions
+
+// simple looping with fixed indices
+function tribonacci(signature,n){  
+  for (var i = 0; i < n-3; i++) { // iterate n times
+    signature.push(signature[i] + signature[i+1] + signature[i+2]); // add last 3 array items and push to trib
+  }
+  return signature.slice(0, n); //return trib - length of n
+}
