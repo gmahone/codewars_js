176af5bdf127d30c2e9f11b437c3282b5d57296d
gmahone <gmahone@gmail.com>
2022-05-30 22:10:27 +0200

    add solution using looped slice

--- tribonacci_sequence.js
+++ tribonacci_sequence.js
@@ -16,3 +16,13 @@ function tribonacci(signature,n){
   }
   return signature.slice(0, n); //return trib - length of n
 }
+
+
+// solution with slice
+function tribonacci(signature,n) {
+  const result = signature.slice(0, n);
+  while (result.length < n) {
+    result[result.length] = result.slice(-3).reduce((p,c) => p + c, 0);
+  }
+  return result;
+}
