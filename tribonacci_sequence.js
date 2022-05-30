e3db058cef2a7c6c53ed7b6c9bda23f550b147ce
gmahone <gmahone@gmail.com>
2022-05-30 22:05:48 +0200

    add initial solution

--- tribonacci_sequence.js
+++ tribonacci_sequence.js
@@ -1,3 +1,8 @@
 function tribonacci(signature,n){
-  //your code here
+  let triSum;
+  for(i = 0; i < (n-3); i++){
+    triSum = signature.filter((a,ind) => ind > i).reduce((acc,c) => acc + c);
+    signature.push(triSum);
+  }
+  return signature;
 }
