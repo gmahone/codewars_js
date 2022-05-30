2ae8a9f3bab565e95acacc26fff34a970f52d406
gmahone <gmahone@gmail.com>
2022-05-30 08:30:45 +0200

    added regex solution

--- dna_to_rna_conversion.js
+++ dna_to_rna_conversion.js
@@ -7,3 +7,10 @@ const DNAtoRNA = dna => dna.replaceAll("T", "U");
 //replaceAll not available for nodeJS v14.x
 // so a little longer solution
 const DNAtoRNA = dna => dna.split("").map(a => a.replace("T", "U")).join("");
+
+
+// other solutions
+// regex solution
+function DNAtoRNA(dna){
+  return dna.replace(/T/g, 'U');
+}
