f9eaf45a7d5ce8525d01fdc7db467b561fff4960
gmahone <gmahone@gmail.com>
2022-05-31 21:42:37 +0200

    add solution using includes

--- filter_out_the_geese.js
+++ filter_out_the_geese.js
@@ -6,3 +6,12 @@ function gooseFilter (birds) {
   }
   return result;
 };
+
+
+// other solutions
+
+// solution using includes
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter(b => !geese.includes(b));
+};
