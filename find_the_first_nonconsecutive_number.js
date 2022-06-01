0553785c8bc6977154f086ec0eb366fb710cbb51
gmahone <gmahone@gmail.com>
2022-06-01 21:10:45 +0200

    initial solution

--- find_the_first_nonconsecutive_number.js
+++ find_the_first_nonconsecutive_number.js
@@ -1,3 +1,9 @@
 function firstNonConsecutive (arr) {
-
+  result = [];
+  for(let i = 1; i < arr.length; i++){
+    if(arr[i] - arr[i-1] > 1){
+      return(arr[i]);
+    }
+  }
+  return result;
 }
