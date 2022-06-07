cebc0ee55f218fb0ec02e20735644c0482e86eec
gmahone <gmahone@gmail.com>
2022-06-07 22:01:00 +0200

    add alternative formulation for initial solution

--- money_money_money.js
+++ money_money_money.js
@@ -6,3 +6,16 @@ function calculateYears(principal, interest, tax, desired) {
     }
     return i;
 }
+
+
+// other solutions
+
+// alternative formulation
+function calculateYears(principal, interest, tax, desired) {
+    let i = 0;
+    while(principal < desired){
+      principal += ((principal * interest) * (1 - tax));
+      i++;
+    }
+    return i;
+}
