bf4f09a6984f7bef6683ec14e9e89d98bab63ea1
gmahone <gmahone@gmail.com>
2022-06-08 22:08:50 +0200

    bug fixing initial solution

--- get_the_middle_character.js
+++ get_the_middle_character.js
@@ -1,6 +1,6 @@
 function getMiddle(s) {
   let result = "";
-  if(s.len & 1){
+  if(s.length & 1){
     result += s[ Math.floor( s.length / 2 )];
   } else {
     result += s[ s.length / 2 - 1 ] + s[ s.length / 2 ];
