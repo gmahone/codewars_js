6ce9dd0c097398832c5dd39af151778bf35d99ec
gmahone <gmahone@gmail.com>
2022-06-08 22:10:39 +0200

    add solution using only slice

--- get_the_middle_character.js
+++ get_the_middle_character.js
@@ -15,3 +15,8 @@ function getMiddle(s) {
 function getMiddle(s) {
   return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
 }
+
+// using only slice
+function getMiddle(s) {
+  return s.slice((s.length-1)/2, s.length/2+1);
+}
