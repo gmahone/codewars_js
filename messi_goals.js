e459347ee647d0c81eed754ac3c06750e82e6806
gmahone <gmahone@gmail.com>
2022-06-15 22:12:45 +0200

    add expandable solution

--- messi_goals.js
+++ messi_goals.js
@@ -3,3 +3,32 @@ var championsLeagueGoals = 10;
 var copaDelReyGoals = 5;
 
 var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+// other solutions
+
+// after adding variables &
+//  this code is expandable
+const goalScoringStatistics = [
+  {
+    competition: 'La Liga',
+    goals: laLigaGoals,
+  },
+  {
+    competition: 'Champions League',
+    goals: championsLeagueGoals,
+  },
+  {
+    competition: 'Copa del Rey',
+    goals: copaDelReyGoals,
+  }
+]
+
+const countGoals = (arr) => {
+  let sum = 0;
+  arr.forEach((item) => {
+    sum += item.goals;
+  });
+  return sum;
+}
+
+var totalGoals = countGoals(goalScoringStatistics);
