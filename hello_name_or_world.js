a9fe3a0969e3ed306c1e8098c18ee515163992b4
gmahone <gmahone@gmail.com>
2022-06-15 22:25:14 +0200

    give original solution with undefined rather than null

--- hello_name_or_world.js
+++ hello_name_or_world.js
@@ -3,6 +3,12 @@ function hello(name) {
   return("Hello, " + resultName + "!");
 }
 
+// alternative with undefined
+function hello(name) {
+  resultName = (name == "" || name == undefined) ? "World" : name[0].toUpperCase() + name.toLowerCase().substring(1);
+  return("Hello, " + resultName + "!");
+}
+
 // other solutions
 
 // arrow function with template literal
