5024c073d4ea92c45a4be5d160219e29b4b51ff9
gmahone <gmahone@gmail.com>
2022-06-15 22:23:04 +0200

    add arrow function solution with template literal

--- hello_name_or_world.js
+++ hello_name_or_world.js
@@ -2,3 +2,9 @@ function hello(name) {
   resultName = (name == "" || name == null) ? "World" : name[0].toUpperCase() + name.toLowerCase().substring(1);
   return("Hello, " + resultName + "!");
 }
+
+// other solutions
+
+// arrow function with template literal
+const hello = name =>
+  `Hello, ${name ? (name[0].toUpperCase() + name.slice(1).toLowerCase()) : 'World'}!`;
