9e425a7cd55f2860e9195bf6bbe6b2f1433a3444
gmahone <gmahone@gmail.com>
2022-06-15 22:09:41 +0200

    bugfix initial solution

--- messi_goals.js
+++ messi_goals.js
@@ -1,5 +1,5 @@
-var laLigaGoals
-var championsLeagueGoals
-var copaDelReyGoals
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
 
 var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
