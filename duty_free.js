73c0ca8258dc4e5ca167a9ea1f09393789980127
gmahone <gmahone@gmail.com>
2022-06-16 22:42:44 +0200

    add simpler formulation

--- duty_free.js
+++ duty_free.js
@@ -1,5 +1,12 @@
 function dutyFree(normPrice, discount, hol){
   let discountPrice = normPrice * discount / 100
-  let result = Math.floor(hol / discountPrice )
+  let result = Math.floor( hol / discountPrice )
   return result;
+}
+
+// add other solutions
+
+// simpler formulation
+function dutyFree(normPrice, discount, hol){
+  return(Math.floor(hol / normPrice / discount * 100))
 }
