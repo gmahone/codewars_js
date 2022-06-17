827ffde900a9b61888500903ea6e2d8d3bbf83af
gmahone <gmahone@gmail.com>
2022-06-17 22:20:41 +0200

    add initial switch solution

--- switch_it_up.js
+++ switch_it_up.js
@@ -1,3 +1,24 @@
 function switchItUp(number){
-//Write your own Code!
+  let result = "Zero";
+  switch(number){
+    case 1:
+      result = "One";
+    case 2:
+      result = "Two";
+    case 3:
+      result = "Three";
+    case 4:
+      result = "Four";
+    case 5:
+      result = "Five";
+    case 6:
+      result = "Six";
+    case 7:
+      result = "Seven";
+    case 8:
+      result = "Eight";
+    case 9:
+      result = "Nine";
+  }
+  return result;
 }
