88df108d1a80c48d6917776f64add0c9373193bb
gmahone <gmahone@gmail.com>
2022-06-17 22:24:01 +0200

    bugfixing original switch statement solution

--- switch_it_up.js
+++ switch_it_up.js
@@ -3,22 +3,31 @@ function switchItUp(number){
   switch(number){
     case 1:
       result = "One";
+      break;
     case 2:
       result = "Two";
+      break;
     case 3:
       result = "Three";
+      break;
     case 4:
       result = "Four";
+      break;
     case 5:
       result = "Five";
+      break;
     case 6:
       result = "Six";
+      break;
     case 7:
       result = "Seven";
+      break;
     case 8:
       result = "Eight";
+      break;
     case 9:
       result = "Nine";
+      break;
   }
   return result;
 }
