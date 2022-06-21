5c5fda6c6ccb96c286a507d449b05292f3b0f82e
gmahone <gmahone@gmail.com>
2022-06-21 22:07:30 +0200

    working on initial solution

--- primes_in_numbers.js
+++ primes_in_numbers.js
@@ -1,3 +1,35 @@
+// remainder = n
+// while remainder != 1
+//  loop i from 1:n
+//  test if i is prime
+//   if prime, see if remainder % i == 0
+//    if remainder % i == 0
+//     add i to array
+//     remainder /= i
+// sort array
+// loop over array, crunching the same numbers and creating output string
+function isPrime(x){
+    let isPrime = true
+    for (let i = 2; i < x; i++) {
+        if (x % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    return isPrime
+}
+
+
 function primeFactors(n){
-    //your code here
+    let divArray = []
+    let i, j, stillPrime
+    while(n != 1){
+        for(i = 2; i < n; i++){
+            if(n % i == 0 && isPrime(n)){
+                remainder /= n
+            }
+        }
+    }
+    console.log(remainder.sort)
+    return remainder
 }
