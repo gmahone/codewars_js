6cf109a023b5a2a6462f96f703fdf331d5ebce95
gmahone <gmahone@gmail.com>
2022-06-22 21:55:11 +0200

    add regex solution

--- vowel_count.js
+++ vowel_count.js
@@ -3,3 +3,10 @@ function getCount(str) {
   let result = vowelArray.reduce((acc,c) => acc + (str.split(c).length - 1), 0);
   return result;
 }
+
+// other solutions
+
+// regex solution
+function getCount(str) {
+  return (str.match(/[aeiou]/ig)||[]).length;
+}
