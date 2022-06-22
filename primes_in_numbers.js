887d208dab4deea049c9de5d4c8ac83d092089e0
gmahone <gmahone@gmail.com>
2022-06-22 20:47:14 +0200

    add solution with prime checking without extra looping

--- primes_in_numbers.js
+++ primes_in_numbers.js
@@ -41,3 +41,16 @@ function primeFactors(n){
     }
     return result.split("**1").join("");
 }
+
+
+// other solutions
+
+// for + while solution
+function primeFactors(n){
+  for (var i=2, res="", f; i <= n; i++) {
+    f=0;
+    while (n%i == 0) { f++; n/=i }
+    res += f ? "(" + ( f>1 ? i+"**"+f  : i ) +")" : ""
+  }
+  return res || "("+n+")"
+}
