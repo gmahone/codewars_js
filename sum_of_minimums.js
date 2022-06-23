7d57a1aff1be6899b49cc45d4a2fd754d4590b4b
gmahone <gmahone@gmail.com>
2022-06-23 21:55:55 +0200

    feat: add solution using reduce first on array of arrays

--- sum_of_minimums.js
+++ sum_of_minimums.js
@@ -2,3 +2,9 @@ function sumOfMinimums(arr) {
   let result = arr.map(a => Math.min(...a)).reduce((acc,c) => acc + c);
   return result;
 }
+
+// using reduce first on array of arrays
+function sumOfMinimums(arr) {
+  let result = arr.reduce((acc, c) => acc + Math.min(...c), 0);
+  return result;
+}
