be19e8e11f8a8cfd50d14cc00ac8af755013f27c
gmahone <gmahone@gmail.com>
2022-06-25 14:06:06 +0200

    fix: working on initial solution

--- reverse_or_rotate.js
+++ reverse_or_rotate.js
@@ -1,3 +1,21 @@
 function revrot(str, sz) {
-    // your code
+    let resultArray = []
+    if(sz > str.length){
+        return "";
+    }
+    let loopLength = str.length % sz;
+    let tempStr;
+    for(let i = 0; i < loopLength; i++){
+        console.log(str);
+        tempStr = str.substring(0, sz).split("");
+        str = str.substring(sz);
+        if(Number(tempStr.reduce((acc,c) => acc + (+c)**3)) % 2 === 0){
+            resultArray.push(tempStr.reverse().join(""));
+        } else {
+            tempStr.push(tempStr.shift());
+            resultArray.push(tempStr.join(""));
+        }
+    }
+    console.log(resultArray)
+    return resultArray.join("");
 }
