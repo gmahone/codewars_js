c54e659b48f2f6957654e21909859fca0bf2c75b
gmahone <gmahone@gmail.com>
2022-06-25 22:30:25 +0200

    feat: add error items to map

--- welcome.js
+++ welcome.js
@@ -1,23 +1,25 @@
 greetingMap = {
-	english: "Welcome",
-	czech: "Vitejte",
-	danish: "Velkomst",
-	dutch: "Welkom",
-	estonian: "Tere tulemast",
-	finnish: "Tervetuloa",
-	flemish: "Welgekomen",
-	french: "Bienvenue",
-	german: "Willkommen",
-	irish: "Failte",
-	italian: "Benvenuto",
-	latvian: "Gaidits",
-	lithuanian: "Laukiamas",
-	polish: "Witamy",
-	spanish: "Bienvenido",
-	swedish: "Valkommen",
-	welsh: "Croeso"
+	"english": "Welcome",
+	"czech": "Vitejte",
+	"danish": "Velkomst",
+	"dutch": "Welkom",
+	"estonian": "Tere tulemast",
+	"finnish": "Tervetuloa",
+	"flemish": "Welgekomen",
+	"french": "Bienvenue",
+	"german": "Willkommen",
+	"irish": "Failte",
+	"italian": "Benvenuto",
+	"latvian": "Gaidits",
+	"lithuanian": "Laukiamas",
+	"polish": "Witamy",
+	"spanish": "Bienvenido",
+	"swedish": "Valkommen",
+	"welsh": "Croeso"
+	"IP_ADDRESS_INVALID": "Welcome",
+	"IP_ADDRESS_NOT_FOUND": "Welcome",
+	"IP_ADDRESS_REQUIRED": "Welcome"
 }
 
 function greet(language) {
-	
 }
