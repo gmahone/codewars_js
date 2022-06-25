c32a6421d869b4efd70070b5ef956403e5de3b57
gmahone <gmahone@gmail.com>
2022-06-25 22:27:08 +0200

    feat: add greeting map

--- welcome.js
+++ welcome.js
@@ -1,3 +1,23 @@
+greetingMap = {
+	english: "Welcome",
+	czech: "Vitejte",
+	danish: "Velkomst",
+	dutch: "Welkom",
+	estonian: "Tere tulemast",
+	finnish: "Tervetuloa",
+	flemish: "Welgekomen",
+	french: "Bienvenue",
+	german: "Willkommen",
+	irish: "Failte",
+	italian: "Benvenuto",
+	latvian: "Gaidits",
+	lithuanian: "Laukiamas",
+	polish: "Witamy",
+	spanish: "Bienvenido",
+	swedish: "Valkommen",
+	welsh: "Croeso"
+}
+
 function greet(language) {
 	
 }
