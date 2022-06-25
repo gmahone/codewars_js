bcdc9cd71e2876ee87d848d143dd294ccd26491d
gmahone <gmahone@gmail.com>
2022-06-25 14:29:53 +0200

    feat: add regex solution

--- reverse_or_rotate.js
+++ reverse_or_rotate.js
@@ -48,3 +48,19 @@ function revrot(str, sz) {
     return arr.filter(a => a.length === sz).map(a => revrotEach(a)).join('');
   }
 }
+
+
+// regex solution
+function revrot(str, sz) {
+  if (sz < 1 || sz > str.length) 
+    return '';
+
+  let reverse = s => s.split('').reverse().join('');
+  let rotate  = s => s.slice(1) + s.slice(0, 1);
+  let sum_cubes = c => c.split('').reduce((a, b) => a + +b ** 3, 0); 
+
+  return str
+    .match(new RegExp('.{' + sz + '}', 'g'))
+    .map(c => sum_cubes(c) % 2 ? rotate(c) : reverse(c))
+    .join('');
+}
