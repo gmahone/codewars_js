8c303f7e54655a602a5b31fbfcf7b512023ed4a5
gmahone <gmahone@gmail.com>
2022-06-26 21:57:40 +0200

    feat: add arrow function solution version

--- apple_turnover.js
+++ apple_turnover.js
@@ -3,3 +3,7 @@ function apple(x){
   let resultUnder = "Help yourself to a honeycomb Yorkie for the glovebox."
   return x**2 > 1000 ? resultOver : resultUnder;
 }
+
+
+// arrow function version
+const apple = x => Math.pow(x) > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
