a5f175324dba059db69a9db83bd9d51ace7a9c8c
gmahone <gmahone@gmail.com>
2022-06-28 21:49:18 +0200

    feat: add arrow function version

--- sum_of_positive.js
+++ sum_of_positive.js
@@ -1,3 +1,6 @@
 function positiveSum(arr) {
   return arr.filter(e => e > 0).reduce((acc,c) => acc + c, 0);
 }
+
+// arrow function version
+const positiveSum = arr => arr.filter(e => e > 0).reduce((acc,c) => acc + c, 0);
