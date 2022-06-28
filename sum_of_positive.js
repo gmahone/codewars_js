93bec0f59c9407b67d50f083858d940a1d4eca9f
gmahone <gmahone@gmail.com>
2022-06-28 21:49:37 +0200

    feat: add solution using a ternary in reduce

--- sum_of_positive.js
+++ sum_of_positive.js
@@ -4,3 +4,8 @@ function positiveSum(arr) {
 
 // arrow function version
 const positiveSum = arr => arr.filter(e => e > 0).reduce((acc,c) => acc + c, 0);
+
+// using only reduce
+function positiveSum(arr) {
+   return arr.reduce((acc,c)=> acc + (c > 0 ? c : 0), 0);
+}
