010f7fa5d768fc7ef4660e29f2b29141c9353bef
gmahone <gmahone@gmail.com>
2022-06-28 22:00:42 +0200

    feat: add similar one line solution

--- odd_or_even.js
+++ odd_or_even.js
@@ -2,3 +2,8 @@ function oddOrEven(array) {
    let result = array.reduce((acc,c) => acc + c, 0);
    return result % 2 == 0 ? "even" : "odd";
 }
+
+// with one line
+function oddOrEven(arr) {
+  return arr.reduce((acc,c) => acc+c, 0) % 2 ? "odd" : "even";
+}
