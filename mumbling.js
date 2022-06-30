b5b93eb9cbb378f64937904a787b8f9f51cf1cf9
gmahone <gmahone@gmail.com>
2022-06-30 14:48:40 +0200

    feat: add initial solution

--- mumbling.js
+++ mumbling.js
@@ -1,3 +1,7 @@
 function accum(s) {
-	return "";
+	let result = s.split("")
+    .map((e,i) => new Array(i+1).fill(e.toLowerCase()).join(""))
+    .map(a => a.slice(0,1).toUpperCase() + a.slice(1))
+    .join("-")
+  return result;
 }
