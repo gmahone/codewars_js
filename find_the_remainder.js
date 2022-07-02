5ce6b19eeef8494cd3f481b4574326291b926819
gmahone <gmahone@gmail.com>
2022-07-02 10:12:50 +0200

    fix: bugs in initial solution

--- find_the_remainder.js
+++ find_the_remainder.js
@@ -1,4 +1,7 @@
 function remainder(n, m){
+  if([n,m].includes(0)){
+    return NaN;
+  }
   let sortedArray = [n,m].sort();
-  return sortedArray[1] % sortedArray[0];
+  return sortedArray[0] % sortedArray[1];
 }
