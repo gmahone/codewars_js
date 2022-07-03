29da9bc3c33f20e5776d236d34a3801e8c3635d9
gmahone <gmahone@gmail.com>
2022-07-03 16:59:36 +0200

    feat: add initial solution

--- pipe_problem.js
+++ pipe_problem.js
@@ -1,3 +1,5 @@
 function pipeFix(numbers){
-  return "";
+  arrayLength = numbers[number.length-1] - numbers[0] + 1;
+  resultArray = new Array(arrayLength).fill(numbers[0]).map((e,i) => e + i);
+  return resultArray;
 }
