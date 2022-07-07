2d29b197b6ac2dc4d6c8ae5fe17392f6776a01c8
Greg Mahone <gmahone@gmail.com>
2022-07-07 21:37:30 +0200

    feat: add simpler solution version

--- gap_in_primes.js
+++ gap_in_primes.js
@@ -20,3 +20,20 @@ function gap(g, m, n) {
     }
     return result;
 }
+
+
+// simpler version
+function gap(g, m, n) {
+    let previousPrime = 0;
+    let isPrime = function(x) { 
+      for (var i=2; i*i<=x; i++) { if (x % i == 0) return false; } return true;
+    }
+    
+    for(var i = m; i <= n; i++)
+        if(isPrime(i)) {
+            if(i - previousPrime == g) return [previousPrime, i];
+            else previousPrime = i;
+        }
+      
+    return null;
+}
