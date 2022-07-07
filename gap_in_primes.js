60683410cb28e8e2a77001eda554c36b72a74285
Greg Mahone <gmahone@gmail.com>
2022-07-07 21:15:09 +0200

    feat: add isPrime function

--- gap_in_primes.js
+++ gap_in_primes.js
@@ -1,3 +1,12 @@
+function isPrime(n){
+    for(let i = 2; i < n; i++){
+        if(n % i > 0){
+            return false;
+        }
+    }
+    return true;
+}
+
 function gap(g, m, n) {
     // your code
 }
