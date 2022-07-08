84ec1adfd298ec4768654908522528a7e401f492
Greg Mahone <gmahone@gmail.com>
2022-07-08 09:17:19 +0200

    feat: add solution using Array.from in Set rather than spread

--- remove_duplicates_from_list.js
+++ remove_duplicates_from_list.js
@@ -3,6 +3,11 @@ function distinct(a) {
   return [...new Set(a)];
 }
 
+// or using Array.from rather than spread
+function distinct(a) {
+  return Array.from(new Set(a));
+}
+
 // using filter
 function distinct(a) {
   return a.filter((e, i, arr) => arr.indexOf(e) === i);
