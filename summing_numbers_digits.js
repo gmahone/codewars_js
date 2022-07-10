5a1cb729b7d47152b6feaf2db50af9798582b6a8
Greg Mahone <gmahone@gmail.com>
2022-07-10 14:34:34 +0200

    feat: add solution version with toString

--- summing_numbers_digits.js
+++ summing_numbers_digits.js
@@ -2,3 +2,6 @@ function sumDigits(number) {
   return String(Math.abs(number)).split("").reduce((acc,c) => acc + +c, 0);
 }
 
+function sumDigits(number) {
+  return Math.abs(number).toString().split("").reduce((acc,c) => acc + +c, 0);
+}
