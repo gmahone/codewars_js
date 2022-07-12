f796c5fa8fbe280e47d81630c85d926af7dde96d
Greg Mahone <gmahone@gmail.com>
2022-07-12 20:43:52 +0200

    feat: add solution with for loop

--- sum_of_multiples.js
+++ sum_of_multiples.js
@@ -9,3 +9,16 @@ function sumMul(n,m){
     .reduce((acc,c) => acc + c,0);
   return result;
 }
+
+
+// solution via for loop
+function sumMul(n,m){
+  if(m < 1){
+    return "INVALID";
+  }
+  var sum = 0;
+  for (var i = n; i < m; i+=n) {
+    sum += i;
+  }
+  return sum;
+}
