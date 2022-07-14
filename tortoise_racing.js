fe15a2a05ee81528ad88bbc16465aa57c9b74caf
Greg Mahone <gmahone@gmail.com>
2022-07-14 21:59:33 +0200

    feat: working on recursive function

--- tortoise_racing.js
+++ tortoise_racing.js
@@ -8,6 +8,7 @@
 //  x = distance traveled by both
 //  x/v1 = (x+g)/v2
 
+/*
 function travelComp(v1, v2, g, s){
   let dist1 = v1/3600 * s + g;
   let dist2 = v2/3600 * s;
@@ -17,6 +18,33 @@ function travelComp(v1, v2, g, s){
       return travelComp(v1, v2, g, s+1);
   }
 }
+*/
+
+// create travelComp functions for Hour Minute and Second
+function travelCompHour(v1, v2, g, h, m, s){
+  let dist1 = v1 * h + g;
+  let dist2 = v2 * h;
+  if(dist2 > dist1){
+    return travelCompMinute(v1, v2, g, h-1, m+1, s);
+  } else if (dist2 == dist1){
+    return [h, 0, 0]
+  } else {
+    return travelCompHour(v1, v2, g, h+1, m, s);
+  }
+}
+
+function travelCompMinute(v1, v2, g, h, m, s){
+  let dist1 = v1 * h + g;
+  let dist2 = v2 * h;
+  if(dist2 > dist1){
+    return travelCompSecond(v1, v2, g, h, m-1, s+1);
+  } else if (dist2 == dist1){
+    return [h, m, 0]
+  } else {
+    return travelCompHour(v1, v2, g, h+1, m, s);
+  }
+}
+
 
 // lets just brute force with recursion...
 function race(v1, v2, g) {
@@ -29,3 +57,4 @@ function race(v1, v2, g) {
           Math.floor((result % 3600) / 60), 
           Math.floor((result % 3600) % 60)];
 }
+
