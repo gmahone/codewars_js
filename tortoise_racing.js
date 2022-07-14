e6ac8182f6b9cb13d1260ad370ac1b71bc3603f6
Greg Mahone <gmahone@gmail.com>
2022-07-14 20:55:30 +0200

    feat: add recursive solution

--- tortoise_racing.js
+++ tortoise_racing.js
@@ -8,6 +8,19 @@
 //  x = distance traveled by both
 //  x/v1 = (x+g)/v2
 
+function travelComp(v1, v2, g, s){
+    let dist1 = v1/3600 * s + g;
+    let dist2 = v2/3600 * s;
+    if(dist2 >= dist1){
+        return s;
+    } else {
+        return travelComp(v1, v2, g, s+1);
+    }
+}
+
+// lets just brute force with recursion...
 function race(v1, v2, g) {
-    // your code
+    let result = travelComp(v1, v2, g, s=0);
+    console.log(result);
+    return "";
 }
