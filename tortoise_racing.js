fad79d43f8c7af4134e6a826283e435aeca04732
Greg Mahone <gmahone@gmail.com>
2022-07-14 20:34:26 +0200

    prep: add description

--- tortoise_racing.js
+++ tortoise_racing.js
@@ -1,3 +1,9 @@
+// DESC:
+//  v1 is the speed of tortoise 1 (feet per hour)
+//  v2 is the speed of tortoise 2 (feet per hour)
+//  g is the headstart of v1 (feet)
+//  if v1 >= v2, return None
+
 function race(v1, v2, g) {
     // your code
 }
