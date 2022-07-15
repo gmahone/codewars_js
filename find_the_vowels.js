7233cc1c9dacbe6d435c83e3377a5b1d9d3874d0
Greg Mahone <gmahone@gmail.com>
2022-07-15 22:36:20 +0200

    feat: add loop solution

--- find_the_vowels.js
+++ find_the_vowels.js
@@ -5,3 +5,14 @@ function vowelIndices(word){
     .map((e,i) => e.match(/[aeiouy]/) ? i+1 : null)
     .filter(e => e != null);
 }
+
+// loop solution
+function vowelIndices(word) {
+  var arr = [];
+  for(var i = 0; i < word.length; i++) {
+    if(/[aeiouy]/i.test(word[i])) {
+      arr.push(i+1);
+    }
+  }
+  return arr;
+}
