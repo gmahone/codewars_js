187f62fa15be2dc3f5defc0997dd01aaa2284bfa
Greg Mahone <gmahone@gmail.com>
2022-07-15 22:34:54 +0200

    fix: add y to vowel list

--- find_the_vowels.js
+++ find_the_vowels.js
@@ -2,6 +2,6 @@ function vowelIndices(word){
   return word
     .toLowerCase()
     .split("")
-    .map((e,i) => e.match(/[aeiou]/) ? i+1 : null)
+    .map((e,i) => e.match(/[aeiouy]/) ? i+1 : null)
     .filter(e => e != null);
 }
