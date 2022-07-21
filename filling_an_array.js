a6f59b91cefdb19e75e0feab162c849ce6f0430e
Greg Mahone <gmahone@gmail.com>
2022-07-21 20:59:50 +0200

    feat: add solution with looping

--- filling_an_array.js
+++ filling_an_array.js
@@ -1,1 +1,11 @@
 const arr = N => N ? Array(N).fill(0).map((e,i) => e + i) : [];
+
+
+// non arrow function via looping
+function arr(n){
+  var newArr = [];
+  for(var i = 0; i < n; i++){
+    newArr.push(i);
+  }
+  return newArr;
+}
