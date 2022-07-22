e71fb87f1f0749fba21b5133b7f2a5d176057ff1
Greg Mahone <gmahone@gmail.com>
2022-07-22 22:02:53 +0200

    feat: add arrow function version

--- array_mean.js
+++ array_mean.js
@@ -1,3 +1,7 @@
 var findAverage = function (nums) {
   return nums.reduce((acc,c) => acc + c, 0)/nums.length
 }
+
+
+// arrow function version
+const findAverage = nums => nums.reduce((acc, c) => acc + c, 0) / nums.length;
