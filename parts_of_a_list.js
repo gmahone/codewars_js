462bd02f9a7546e10c4d606066495ea5fd35e39d
Greg Mahone <gmahone@gmail.com>
2022-07-23 13:09:38 +0200

    fix: switch to looping solution

--- parts_of_a_list.js
+++ parts_of_a_list.js
@@ -1,3 +1,12 @@
+//function partlist(arr) {
+//    return arr.map((e,i,arr) => [arr.slice(0,i+1).join(" "), arr.slice(i+1).join(" ")])
+//}
+
+
 function partlist(arr) {
-    return arr.map((e,i,arr) => [arr.slice(0,i+1).join(" "), arr.slice(i+1).join(" ")])
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        result.push([arr.slice(0,i+1).join(" "), arr.slice(i+1).join(" ")]);
+    }
+    return result;
 }
