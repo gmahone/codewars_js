2bde7c5a64c4f8f22fe375b121d0960c5b187f5a
Greg Mahone <gmahone@gmail.com>
2022-07-24 21:52:11 +0200

    feat: add solution that attaches from obj

--- basic_datatypes_objects.js
+++ basic_datatypes_objects.js
@@ -9,3 +9,8 @@ const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 function animal({name, legs, color}) {
   return `This ${color} ${name} has ${legs} legs.`;
 }
+
+// solution attached variables from obj
+function animal(obj){
+  let {color,name,legs} = obj;
+  return `This ${color} ${name} has ${legs} legs.`
