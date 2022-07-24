1b0c220446ca7f06a202c67c958037d3e621fe89
Greg Mahone <gmahone@gmail.com>
2022-07-24 21:50:25 +0200

    feat: add arrow function version

--- basic_datatypes_objects.js
+++ basic_datatypes_objects.js
@@ -2,3 +2,5 @@ function animal(obj){
   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
 
+// arrow function
+const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
