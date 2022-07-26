5d435a3e535abd5f221119132f4790b2eef2bc4c
Greg Mahone <gmahone@gmail.com>
2022-07-26 09:06:27 +0200

    feat: add arrow function version

--- name_shuffler.js
+++ name_shuffler.js
@@ -2,3 +2,6 @@ function nameShuffler(str){
   let res = str.split(" ").reverse().join(" ");
   return res;
 }
+
+// arrow function version
+const nameShuffler = str => str.split(" ").reverse().join(" ");
