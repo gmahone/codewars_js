6e193a8eb6f82ae2d94e753f766f90de14acf86b
Greg Mahone <gmahone@gmail.com>
2022-08-13 22:03:51 +0200

    feat: add formulaic solution

--- sums_of_lots_of_numbers.js
+++ sums_of_lots_of_numbers.js
@@ -5,3 +5,9 @@ function f(n){
   let result = Array(n).fill(1).map((e, i) => e + i).reduce((acc, c) => acc + c, 0);
   return result;
 };
+
+
+// formulaic
+function f(n){
+  return (parseInt(n) === n && n > 0) ? n*(n+1)/2 : false;
+};
