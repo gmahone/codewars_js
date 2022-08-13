b2b5d885cc0d190bc3814b7325185529a3a84f20
Greg Mahone <gmahone@gmail.com>
2022-08-13 22:03:11 +0200

    fix: edge case of input less than 1

--- sums_of_lots_of_numbers.js
+++ sums_of_lots_of_numbers.js
@@ -1,5 +1,5 @@
 function f(n){
-  if(parseInt(n) !== n){
+  if(parseInt(n) !== n || n < 1){
     return false;
   }
   let result = Array(n).fill(1).map((e, i) => e + i).reduce((acc, c) => acc + c, 0);
