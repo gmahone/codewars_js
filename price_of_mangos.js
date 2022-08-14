6c61066d0544ef6685e8d9e0e0daedac5c1dde2b
Greg Mahone <gmahone@gmail.com>
2022-08-14 21:07:06 +0200

    fix: add parseInt to remove partials

--- price_of_mangos.js
+++ price_of_mangos.js
@@ -1,5 +1,6 @@
 function mango(quantity, price){
+  console.log(quantity, price);
   let result1 = quantity % 3 * price;
-  let result2 = 2 * quantity / 3 * price;
+  let result2 = 2 * parseInt(quantity / 3) * price;
   return (result1 + result2);
 }
