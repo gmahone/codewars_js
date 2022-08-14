5e50b077eb3db55a70341158efeb9959f3831ebe
Greg Mahone <gmahone@gmail.com>
2022-08-14 21:06:11 +0200

    fix: working on edge cases still

--- price_of_mangos.js
+++ price_of_mangos.js
@@ -1,4 +1,5 @@
 function mango(quantity, price){
-  console.log(quantity, price);
-  return (2 * quantity / 3 * price);
+  let result1 = quantity % 3 * price;
+  let result2 = 2 * quantity / 3 * price;
+  return (result1 + result2);
 }
