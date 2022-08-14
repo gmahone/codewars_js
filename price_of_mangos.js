9e7baa9691f5d2c0f6e91a3ca29b30b996723d85
Greg Mahone <gmahone@gmail.com>
2022-08-14 21:08:17 +0200

    feat: add solution with cleaner math

--- price_of_mangos.js
+++ price_of_mangos.js
@@ -3,3 +3,9 @@ function mango(quantity, price){
   let result2 = 2 * parseInt(quantity / 3) * price;
   return (result1 + result2);
 }
+
+
+// solution with cleaner math
+function mango(quantity, price){
+  return price * (quantity - Math.floor(quantity / 3));
+}
