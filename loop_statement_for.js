2c0d6a5d18462bc1834c207110ee3f43270c4181
Greg Mahone <gmahone@gmail.com>
2022-08-14 20:56:37 +0200

    feat: add ternary array selector solution

--- loop_statement_for.js
+++ loop_statement_for.js
@@ -9,3 +9,17 @@ function pickIt(arr){
   }
   return [odd,even];
 }
+
+
+// ternary array return solution
+function pickIt (arr) {
+  
+  let odd = []
+  let even =[]
+      
+  for (var x of arr) {
+      ((x % 2) ? odd : even).push(x)    
+  }
+      
+  return [odd, even]
+}
