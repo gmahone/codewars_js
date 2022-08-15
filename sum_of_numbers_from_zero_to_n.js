011f7f518145d74193f7efa303051bdbecfb64c3
Greg Mahone <gmahone@gmail.com>
2022-08-15 20:53:42 +0200

    feat: add initial solution

--- sum_of_numbers_from_zero_to_n.js
+++ sum_of_numbers_from_zero_to_n.js
@@ -2,8 +2,7 @@ var SequenceSum = (function() {
   function SequenceSum() {}
 
   SequenceSum.showSequence = function(count) {
-    // for
-      //while
+    return count > 0 ? Array(count).fill(1).map((e,i) => e + i).reduce((acc, c) => acc + c, 0) : 0;
   };
 
   return SequenceSum;
