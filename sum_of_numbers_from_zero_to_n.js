bf4535d1533f4cc6a4760a59e39972bd5938dda8
Greg Mahone <gmahone@gmail.com>
2022-08-15 21:02:20 +0200

    fix: working on initial solution

--- sum_of_numbers_from_zero_to_n.js
+++ sum_of_numbers_from_zero_to_n.js
@@ -2,7 +2,19 @@ var SequenceSum = (function() {
   function SequenceSum() {}
 
   SequenceSum.showSequence = function(count) {
-    return count > 0 ? Array(count).fill(1).map((e,i) => e + i).reduce((acc, c) => acc + c, 0) : 0;
+
+    let result1 = Array(count+1).fill(0).map((e,i) => e + i);
+    console.log(count)
+    
+    if(count < 0){
+      return `${count}<0`;
+    }
+    if(count === 0){
+      return "0=0";
+    }
+    
+    
+    return result1.join("+") + " = " + result1.reduce((acc, c) => acc + c, 0);
   };
 
   return SequenceSum;
