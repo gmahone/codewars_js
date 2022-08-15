4db9575cf2ca118c1810770bc6bb4d53aa6e5792
Greg Mahone <gmahone@gmail.com>
2022-08-15 21:03:39 +0200

    feat: add solution using for looping

--- sum_of_numbers_from_zero_to_n.js
+++ sum_of_numbers_from_zero_to_n.js
@@ -18,3 +18,30 @@ var SequenceSum = (function() {
   return SequenceSum;
 
 })();
+
+
+// looping solution
+var SequenceSum = (function() {
+  function SequenceSum() {}
+
+  SequenceSum.showSequence = function(count) {
+    var sum = 0;
+    var str = "";
+    if (count === 0) {
+      return "0=0";
+    } else if (count < 0) {
+      return count + "<0";
+    } else {
+      for (var i = 0; i < count; i++) {
+        sum += i;
+        str += i + "+";
+      };
+    sum += count;
+    str += count + " = " + sum;
+    return str;
+    };
+  };
+
+  return SequenceSum;
+
+})();
