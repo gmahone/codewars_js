cbc8e3e4e77bf2688e372c801d7333daa67b15e3
Greg Mahone <gmahone@gmail.com>
2022-08-16 20:41:38 +0200

    feat: add recursive function solution

--- sum_of_cubes.js
+++ sum_of_cubes.js
@@ -1,3 +1,13 @@
 function sumCubes(n){
   return Array(n).fill(1).map((e,i) => e + i).reduce((acc,c) => acc + c**3, 0);
 }
+
+
+// recursive function
+function sumCubes(n){
+  if (n == 1) {
+    return n**3;
+  } else {
+    return n**3 + sumCubes(n - 1);
+  }
+}
