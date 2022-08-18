c91ea8c9aac552c9911cc3c5f77bc7dd5e64d77e
Greg Mahone <gmahone@gmail.com>
2022-08-18 21:14:37 +0200

    feat: add solution using making a string method capitalize

--- greet_me.js
+++ greet_me.js
@@ -1,3 +1,13 @@
 var greet = function(name) {
   return `Hello ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!`;
 };
+
+
+// add string method capitalize
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+}
+
+var greet = function(name) {
+return "Hello " + name.capitalize() + "!";
+};
