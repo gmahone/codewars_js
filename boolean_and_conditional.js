ba28e14fa8eaf56f65cd3d9a26155fcfd24d9794
Greg Mahone <gmahone@gmail.com>
2022-08-19 21:11:21 +0200

    feat: add solution converting to bool then string

--- boolean_and_conditional.js
+++ boolean_and_conditional.js
@@ -2,3 +2,9 @@ function trueOrFalse(val){
   if (val)    return "true";             
   else     return "false";
 }
+
+
+// different version
+function trueOrFalse(val){
+  return Boolean(val).toString();
+}
