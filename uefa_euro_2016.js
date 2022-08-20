95b31f62e2870c2d28a3ab5eb950e5fc7d044382
Greg Mahone <gmahone@gmail.com>
2022-08-20 21:19:04 +0200

    feat: add solution that predefines teams, scores, winner

--- uefa_euro_2016.js
+++ uefa_euro_2016.js
@@ -9,3 +9,15 @@ function uefaEuro2016(teams, scores){
   }
   return result;
 }
+
+
+// define winner first
+function uefaEuro2016(teams, scores) {
+    const [team1, team2] = teams
+    const [score1, score2] = scores
+    const winner = score1 > score2 ? team1 : team2
+
+    return score1 === score2
+        ? `At match ${team1} - ${team2}, teams played draw.`
+        : `At match ${team1} - ${team2}, ${winner} won!`
+}
