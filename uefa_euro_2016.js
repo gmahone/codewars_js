62a34b22aa21a2776af554d1e6d36bb1e8fbb56f
Greg Mahone <gmahone@gmail.com>
2022-08-20 21:17:25 +0200

    feat: add initial solution

--- uefa_euro_2016.js
+++ uefa_euro_2016.js
@@ -1,3 +1,11 @@
 function uefaEuro2016(teams, scores){
-  return "";
+  let result = `At match ${teams[0]} - ${teams[1]}, `
+  if(scores[0] > scores[1]){
+    result += `${teams[0]} won!`;
+  } else if(scores[0] < scores[1]){
+    result += `${teams[1]} won!`;
+  } else {
+    result += "teams played draw."
+  }
+  return result;
 }
