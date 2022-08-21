16dd2dd8a2673b9ea70b515b323ecc7cf24102de
Greg Mahone <gmahone@gmail.com>
2022-08-21 21:17:52 +0200

    fix: edge case solution

--- duck_duck_goose.js
+++ duck_duck_goose.js
@@ -1,5 +1,8 @@
 function duckDuckGoose(players, goose) {
-  let chosen = players.length % goose;
-  console.log(chosen)
+  let chosen = (goose % players.length) - 1;
+  if(goose % players.length === 0){
+    chosen = players.length - 1
+  }
+  console.log(players.length, goose, chosen)
   return players[chosen].name;
 }
