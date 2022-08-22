631f79c1a881061907602d58ab9b28019f8fb27c
Greg Mahone <gmahone@gmail.com>
2022-08-22 17:01:51 +0200

    feat: add string addition solution

--- welcome_to_the_city.js
+++ welcome_to_the_city.js
@@ -1,3 +1,9 @@
 function sayHello( name, city, state ) {
   return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
 }
+
+
+// via string addition
+function sayHello( name, city, state ) {
+  return 'Hello, ' + name.join(' ') + '! Welcome to ' + city + ', ' + state + '!';
+}
