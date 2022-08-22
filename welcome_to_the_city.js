1d453e8d0ac21fdde9c6f7cb4dcc54e20bb88e44
Greg Mahone <gmahone@gmail.com>
2022-08-22 17:02:33 +0200

    feat: add arrow function solution

--- welcome_to_the_city.js
+++ welcome_to_the_city.js
@@ -7,3 +7,7 @@ function sayHello( name, city, state ) {
 function sayHello( name, city, state ) {
   return 'Hello, ' + name.join(' ') + '! Welcome to ' + city + ', ' + state + '!';
 }
+
+
+// arrow function
+const sayHello = ( name, city, state ) => `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`
