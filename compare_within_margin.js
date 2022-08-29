91e7a83c4a839ca89d926bdb46aa2f9f8a6088b4
Greg Mahone <gmahone@gmail.com>
2022-08-29 20:30:16 +0200

    feat: add two cases to initial solution

--- compare_within_margin.js
+++ compare_within_margin.js
@@ -1,4 +1,11 @@
-function closeCompare(a, b, margin){
-  return "";
+function closeCompare(a, b, margin = 0){
+  let result;
+  if(a > b + margin){
+    result = 1;
+  }
+  if(a < b + margin){
+    result = -1;
+  }
+    
 }
 
