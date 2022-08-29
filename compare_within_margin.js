6497e9c62e58d2191cb2cb2efbd540a121771f77
Greg Mahone <gmahone@gmail.com>
2022-08-29 20:40:51 +0200

    fix: formatting

--- compare_within_margin.js
+++ compare_within_margin.js
@@ -1,13 +1,11 @@
 function closeCompare(a, b, margin = 0){
   let result = 0;
   let remainder = Math.abs(a - b);
-  console.log(a,b,margin);
   if(remainder > margin){
-    
     if(a > b){
       result = 1;
     }
-    else {
+    if(a < b){
       result = -1;
     }
   }
