7dec1b145e891817e48453a96e8a612779604409
Greg Mahone <gmahone@gmail.com>
2022-08-29 20:41:36 +0200

    feat: add a solution using a ternary and Math.sign

--- compare_within_margin.js
+++ compare_within_margin.js
@@ -12,3 +12,7 @@ function closeCompare(a, b, margin = 0){
   return result;
 }
 
+// using a ternary and Math.sign
+function closeCompare(a, b, m = 0){
+  return Math.abs(a - b) <= m? 0: Math.sign(a - b);
+}
