cf00d0de6cae4ad13d023edf3284f59ad69d0680
Greg Mahone <gmahone@gmail.com>
2022-09-03 20:53:44 +0200

    feat: add solution using array methods rather than loops

--- array_helpers.js
+++ array_helpers.js
@@ -61,3 +61,12 @@ Array.prototype.odd = function () {
     }
     return result;
 };
+
+
+// using Array methods rather than loops
+Array.prototype.square  = function () { return this.map(function(n) { return n*n; }); }
+Array.prototype.cube    = function () { return this.map(function(n) { return n*n*n; }); }
+Array.prototype.average = function () { return this.sum() / this.length; }
+Array.prototype.sum     = function () { return this.reduce(function(a, b) { return a + b; }, 0); }
+Array.prototype.even    = function () { return this.filter(function(item) { return 0 == item % 2; }); }
+Array.prototype.odd     = function () { return this.filter(function(item) { return 0 != item % 2; }); }
