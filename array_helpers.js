921a628d973261d4ae5ae68830af7e3f6a85a3e6
Greg Mahone <gmahone@gmail.com>
2022-09-03 20:44:26 +0200

    feat: initialize placeholder functions

--- array_helpers.js
+++ array_helpers.js
@@ -1,1 +1,22 @@
+//square
 
+
+//cube
+
+
+//average
+
+
+//sum
+Array.prototype.sum = function () {
+    var total = 0;
+    for (var i = 0; i < this.length; i++) {
+        total += this[i];
+    }
+    return total;
+};
+
+//even
+
+
+//odd
