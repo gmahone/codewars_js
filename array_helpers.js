5f9cc4230b74207e100194e527a6fbb8a892fb4e
Greg Mahone <gmahone@gmail.com>
2022-09-03 20:54:27 +0200

    feat: solution using Object.assign for all at once assigment

--- array_helpers.js
+++ array_helpers.js
@@ -70,3 +70,14 @@ Array.prototype.average = function () { return this.sum() / this.length; }
 Array.prototype.sum     = function () { return this.reduce(function(a, b) { return a + b; }, 0); }
 Array.prototype.even    = function () { return this.filter(function(item) { return 0 == item % 2; }); }
 Array.prototype.odd     = function () { return this.filter(function(item) { return 0 != item % 2; }); }
+
+
+// using Object.assign
+Object.assign(Array.prototype, {
+    square() {return this.map(n => n * n);},
+    cube() {return this.map(n => Math.pow(n, 3));},
+    sum() {return this.reduce((p,n) => p + n, 0);},
+    average() {return this.reduce((p,n) => p + n, 0) / this.length;},
+    even() {return this.filter(n => !(n % 2));},
+    odd() {return this.filter(n => n % 2);}
+});
