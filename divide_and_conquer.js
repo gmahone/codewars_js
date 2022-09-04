f4c0b0ba61f3779620ff0cc2ae6016c358c1fbf6
Greg Mahone <gmahone@gmail.com>
2022-09-04 20:36:29 +0200

    feat: remove console log of solution

--- divide_and_conquer.js
+++ divide_and_conquer.js
@@ -1,7 +1,6 @@
 function divCon(x){
   let stringArraySum = x.filter((e) => typeof(e) == "string").reduce((acc,c) => acc + +c, 0);
   let numberArraySum = x.filter((e) => typeof(e) != "string").reduce((acc,c) => acc + c, 0);
-  console.log(x, stringArraySum, numberArraySum);
   
   return numberArraySum - stringArraySum;
 }
