2ffb0c57f57f58ddc9f2cb92337f0972fcca0c59
Greg Mahone <gmahone@gmail.com>
2022-09-04 20:33:29 +0200

    fix: conversion of string to numeric for sum

--- divide_and_conquer.js
+++ divide_and_conquer.js
@@ -1,5 +1,5 @@
 function divCon(x){
-  let stringArraySum = x.filter((e) => typeof(e) == "string").reduce((acc,c) => acc + c, 0);
+  let stringArraySum = x.filter((e) => typeof(e) == "string").reduce((acc,c) => acc + +c, 0);
   let numberArray = x.filter((e) => typeof(e) != "string")
   console.log(x, stringArraySum, numberArray);
   
