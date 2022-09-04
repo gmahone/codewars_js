b21441066da3fc08dcc4bfde6f3a477b3a5f6bbb
Greg Mahone <gmahone@gmail.com>
2022-09-04 20:34:11 +0200

    feat: add sum of number array

--- divide_and_conquer.js
+++ divide_and_conquer.js
@@ -1,7 +1,7 @@
 function divCon(x){
   let stringArraySum = x.filter((e) => typeof(e) == "string").reduce((acc,c) => acc + +c, 0);
-  let numberArray = x.filter((e) => typeof(e) != "string")
-  console.log(x, stringArraySum, numberArray);
+  let numberArraySum = x.filter((e) => typeof(e) != "string").reduce((acc,c) => acc + c, 0);
+  console.log(x, stringArraySum, numberArraySum);
   
   return "";
 }
