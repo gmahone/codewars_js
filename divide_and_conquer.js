281e53c09815694023d78c9a855e4d6adf9e7edd
Greg Mahone <gmahone@gmail.com>
2022-09-04 20:32:47 +0200

    fix: variable name for string Array sum

--- divide_and_conquer.js
+++ divide_and_conquer.js
@@ -1,7 +1,7 @@
 function divCon(x){
   let stringArraySum = x.filter((e) => typeof(e) == "string").reduce((acc,c) => acc + c, 0);
   let numberArray = x.filter((e) => typeof(e) != "string")
-  console.log(x, stringArray, numberArray);
+  console.log(x, stringArraySum, numberArray);
   
   return "";
 }
