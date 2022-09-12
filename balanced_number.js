223d36ead4d546b143ab2c4cbcc367435b64b3ed
Greg Mahone <gmahone@gmail.com>
2022-09-12 20:54:02 +0200

    fix: variable names in solution

--- balanced_number.js
+++ balanced_number.js
@@ -1,5 +1,5 @@
 function balancedNum(number){
     let numberArray = number.toString().split("").map(e => Number(e));
-    let balanced = numberArray.slice(0,Math.floor(number.length/2)) === numberArray.reverse().slice(0,Math.floor(number.length/2));
+    let balanced = numberArray.slice(0,Math.floor(numberArray.length/2)) === numberArray.reverse().slice(0,Math.floor(numberArray.length/2));
     return balanced ? "Balanced" : "Not balanced";
 }
