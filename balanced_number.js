0588761bb4df36e2cc0bc768f07dbfd06acf7aaa
Greg Mahone <gmahone@gmail.com>
2022-09-12 20:51:19 +0200

    feat: working on initial solution, getting an number array

--- balanced_number.js
+++ balanced_number.js
@@ -1,3 +1,6 @@
 function balancedNum(number){
+    let numberArray = number.toString().split("").map(e => Number(e));
+    console.log(numberArray);
+    //number.slice(0,Math.floor(number.length/2))
     return "";
 }
