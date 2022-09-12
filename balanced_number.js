30f02cbdd56c0e954f7c656f87999de4686cdefe
Greg Mahone <gmahone@gmail.com>
2022-09-12 21:03:51 +0200

    fix: working on indexing values

--- balanced_number.js
+++ balanced_number.js
@@ -1,5 +1,20 @@
 function balancedNum(number){
     let numberArray = number.toString().split("").map(e => Number(e));
-    let balanced = numberArray.slice(0,Math.floor(numberArray.length/2)) == numberArray.reverse().slice(0,Math.floor(numberArray.length/2));
+    let balanced = numberArray
+        .slice(0,Math.floor((numberArray.length-1)/2))
+        .reduce((acc,c) => acc + c, 0) == 
+      numberArray
+        .reverse()
+        .slice(0,Math.floor((numberArray.length-1)/2))
+        .reduce((acc,c) => acc + c, 0);
+    console.log(numberArray
+        .slice(0,Math.floor((numberArray.length-1)/2))
+        .reduce((acc,c) => acc + c, 0))
+    console.log(numberArray
+        .reverse()
+        .slice(0,Math.floor((numberArray.length-1)/2))
+        .reduce((acc,c) => acc + c, 0))
+    console.log(number)
+    console.log(balanced)
     return balanced ? "Balanced" : "Not Balanced";
 }
