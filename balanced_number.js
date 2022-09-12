b3aa4fb0971c64b15ca7388091a5fea491c7be19
Greg Mahone <gmahone@gmail.com>
2022-09-12 20:53:06 +0200

    feat: add initial solution

--- balanced_number.js
+++ balanced_number.js
@@ -1,6 +1,5 @@
 function balancedNum(number){
     let numberArray = number.toString().split("").map(e => Number(e));
-    console.log(numberArray);
-    //number.slice(0,Math.floor(number.length/2))
-    return "";
+    let balanced = numberArray.slice(0,Math.floor(number.length/2)) === numberArray.reverse().slice(0,Math.floor(number.length/2));
+    return balanced ? "Balanced" : "Not balanced";
 }
