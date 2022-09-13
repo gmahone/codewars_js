d08d445df0c4f46e5efefab3b571ba1f8228d6a3
Greg Mahone <gmahone@gmail.com>
2022-09-13 21:13:22 +0200

    feat: add big int solution for large numbers

--- plus_one_array.js
+++ plus_one_array.js
@@ -8,3 +8,17 @@ function upArray(arr){
   let resultArray = String(arrayAsNumber + 1).split("").map(e => Number(e));
   return resultArray;
 }
+
+
+// solution for Big numbers -  requires >= node v10
+function upArray(arr){
+  console.log(arr)
+  let testArray = arr.filter(e => e >= 0 && e <= 9);
+  if(testArray.length != arr.length || arr.length == 0){
+    return null;
+  }
+  let arrayAsNumber = BigInt(arr.map(e => String(e)).join(""));
+  console.log(arrayAsNumber)
+  let resultArray = String(arrayAsNumber + 1n).split("").map(e => Number(e));
+  return resultArray;
+}
