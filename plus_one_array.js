9cc3bb635e12dbac294918b692136681cc058714
Greg Mahone <gmahone@gmail.com>
2022-09-13 20:52:30 +0200

    feat: second produce result as n + 1 back to array

--- plus_one_array.js
+++ plus_one_array.js
@@ -1,5 +1,7 @@
 function upArray(arr){
   let arrayAsNumber = Number(arr.map(e => String(e)).join(""))
   console.log(arrayAsNumber);
+  let resultArray = String(arrayAsNumber + 1).split("").map(e => Number(e));
+  console.log(resultArray);
   return "";
 }
