429b0c3aa7b40dee29f89b26aa8b3b5e6351ce94
Greg Mahone <gmahone@gmail.com>
2022-09-13 20:57:50 +0200

    fix: add edge case test

--- plus_one_array.js
+++ plus_one_array.js
@@ -1,4 +1,8 @@
 function upArray(arr){
+  let testArray = arr.filter(e => e >= 0 && e <= 9)
+  if(testArray.length != arr.length){
+    return null;
+  }
   let arrayAsNumber = Number(arr.map(e => String(e)).join(""))
   console.log(arrayAsNumber);
   let resultArray = String(arrayAsNumber + 1).split("").map(e => Number(e));
