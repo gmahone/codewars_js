56725e071c1f4a70ee4c3a30bbef6cc6c3feb524
Greg Mahone <gmahone@gmail.com>
2022-09-13 20:58:34 +0200

    feat: fix style

--- plus_one_array.js
+++ plus_one_array.js
@@ -1,9 +1,9 @@
 function upArray(arr){
-  let testArray = arr.filter(e => e >= 0 && e <= 9)
+  let testArray = arr.filter(e => e >= 0 && e <= 9);
   if(testArray.length != arr.length){
     return null;
   }
-  let arrayAsNumber = Number(arr.map(e => String(e)).join(""))
+  let arrayAsNumber = Number(arr.map(e => String(e)).join(""));
   let resultArray = String(arrayAsNumber + 1).split("").map(e => Number(e));
   return resultArray;
 }
