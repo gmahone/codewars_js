e0c108a9482c56f95bd6c140be24099cf271a959
Greg Mahone <gmahone@gmail.com>
2022-09-16 21:02:23 +0200

    feat: add solution with string addition

--- combine_strings.js
+++ combine_strings.js
@@ -5,3 +5,9 @@ function combineNames(first, last){
 
 // arrow function with spread and join
 const combineNames = (...names) => names.join(' ');
+
+
+// string addition
+function combineNames(first, last) {
+  return first + " " + last;
+}
