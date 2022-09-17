5d8ef6096a2184ab35c9a4e5ab88e700ca91de1c
Greg Mahone <gmahone@gmail.com>
2022-09-17 15:26:31 +0200

    feat: add solution with for loop

--- power.js
+++ power.js
@@ -1,3 +1,13 @@
 function numberToPower(number, power){
   return new Array(power).fill(number).reduce((acc,c) => acc * c, 1);
 }
+
+
+// using for loop
+function numberToPower(number, power){
+  var total = 1;
+  for (var i = 1; i <= power; i++) { 
+    total = total * number;
+  }
+  return total;
+}
