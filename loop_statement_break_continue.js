72440e3a8e2943c7826406e18cb7cf43c06e1a77
Greg Mahone <gmahone@gmail.com>
2022-09-24 15:55:29 +0200

    feat: add alternative solution

--- loop_statement_break_continue.js
+++ loop_statement_break_continue.js
@@ -12,3 +12,15 @@ function grabDoll(dolls){
   } 
   return bag;
 }
+
+
+// alternative solution
+function grabDoll(dolls) {
+  var bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] !== "Hello Kitty" && dolls[i] !== "Barbie doll") continue;
+    bag.push(dolls[i]);
+    if (bag.length === 3) break;
+  }
+  return bag;
+}
