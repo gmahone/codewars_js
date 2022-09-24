7d52ac259a04241bafdd3512952078c2406fe068
Greg Mahone <gmahone@gmail.com>
2022-09-24 15:53:33 +0200

    fix: debug via console.log

--- loop_statement_break_continue.js
+++ loop_statement_break_continue.js
@@ -2,6 +2,7 @@ function grabDoll(dolls){
   var bag=[];
   
   for(let doll of dolls){
+    console.log(doll, bag);
     if(doll == "Hello Kitty" || doll == "Barbie doll"){
       bag.push(doll);
       if(bag.length > 2){
