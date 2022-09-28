25bb9996b11efa081b78bec8266c8f94793acdc9
Greg Mahone <gmahone@gmail.com>
2022-09-28 22:00:12 +0200

    fix: loop semicolons replaced commas

--- consecutive_strings.js
+++ consecutive_strings.js
@@ -1,7 +1,7 @@
 function longestConsec(strarr, k) {
     let longestWord = "";
     
-    for(let i = 0, i < (strarr.length - k), i++){
+    for(let i = 0; i < (strarr.length - k); i++){
         let tempWord = strarr.slice(i, i+k).join("");
         console.log(tempWord);
         if(tempWord.length > longestWord.length){
