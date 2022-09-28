4e1542c0f516b8274e6dd95158b56936d0608213
Greg Mahone <gmahone@gmail.com>
2022-09-28 22:00:51 +0200

    fix: edge case for negative k

--- consecutive_strings.js
+++ consecutive_strings.js
@@ -1,13 +1,14 @@
 function longestConsec(strarr, k) {
     let longestWord = "";
-    
+    if(k < 1){
+        return(longestWord);
+    }    
     for(let i = 0; i < (strarr.length - k); i++){
         let tempWord = strarr.slice(i, i+k).join("");
         console.log(tempWord);
         if(tempWord.length > longestWord.length){
             longestWord = tempWord;
         }
-        
     }
     return(longestWord);
 }
