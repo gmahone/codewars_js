3193fb19c40f566907b7e57bf06df9db7d2953a2
Greg Mahone <gmahone@gmail.com>
2022-09-28 22:01:19 +0200

    fix: loop extent

--- consecutive_strings.js
+++ consecutive_strings.js
@@ -3,7 +3,7 @@ function longestConsec(strarr, k) {
     if(k < 1){
         return(longestWord);
     }    
-    for(let i = 0; i < (strarr.length - k); i++){
+    for(let i = 0; i < (strarr.length - k + 1); i++){
         let tempWord = strarr.slice(i, i+k).join("");
         console.log(tempWord);
         if(tempWord.length > longestWord.length){
