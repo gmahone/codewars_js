7b1aeb004b8aaa1d76caf4c51be5af2292d37a7c
Greg Mahone <gmahone@gmail.com>
2022-09-28 22:03:02 +0200

    feat: add cleaner solution version

--- consecutive_strings.js
+++ consecutive_strings.js
@@ -11,3 +11,17 @@ function longestConsec(strarr, k) {
     }
     return(longestWord);
 }
+
+
+// cleaner solution
+function longestConsec(strarr, k) {
+    var longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+      var tempArray = strarr.slice(i,i+k);
+      var tempStr = tempArray.join("");
+      if(tempStr.length > longest.length){
+        longest = tempStr;
+      }
+    }
+    return longest;
+}
