776a3600f0eea08cda62e6bf3455b3c93e527241
Greg Mahone <gmahone@gmail.com>
2022-09-29 20:57:25 +0200

    feat: add arrow function solutions

--- basic_data_types_array.js
+++ basic_data_types_array.js
@@ -16,3 +16,15 @@ function popElement(arr){
   arr.pop();
   return(arr);
 }
+
+
+// as arrow functions using less array methods
+const getLength = array => array.length;
+
+const getFirst = array => array[0];
+
+const getLast = array => array[array.length - 1];
+
+const pushElement = array => array.concat(0);
+
+const popElement = array => array.splice(0, array.length - 1);
