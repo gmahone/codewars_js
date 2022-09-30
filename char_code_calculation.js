2306cb95c0883320175b25bb6dce72ae9bf098da
Greg Mahone <gmahone@gmail.com>
2022-09-30 11:54:56 +0200

    fix: prepare for subtraction

--- char_code_calculation.js
+++ char_code_calculation.js
@@ -2,10 +2,15 @@ function calc(x){
   let charCodeArray = String(x).split("")
     .map(e => String(e.charCodeAt()))
     .join("")
+    .split("");
+    
+  let charCodeArraySum = charCodeArray.reduce((acc,c) => acc + +c, 0);
+  
+  let charCodeSwapSum = charCodeArray.join("")
     .replace("7", "1")
     .split("")
     .reduce((acc,c) => acc + +c, 0);
-  console.log(charCodeArray);
+  console.log(charCodeArray, charCodeArraySum, charCodeSwapSum);
   
   return "";
 }
