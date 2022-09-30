21251615f6a4d479747228fbe71d5003cfc557d9
Greg Mahone <gmahone@gmail.com>
2022-09-30 11:49:36 +0200

    fix: summing split char code swaps

--- char_code_calculation.js
+++ char_code_calculation.js
@@ -1,5 +1,10 @@
 function calc(x){
-  let charCodeArray = String(x).split("").map(e => String(e.charCodeAt())).join("").split("");
+  let charCodeArray = String(x).split("")
+    .map(e => String(e.charCodeAt()))
+    .join("")
+    .replace("7", "1")
+    .split("")
+    .reduce((acc,c) => acc + +c, 0);
   console.log(charCodeArray);
   
   return "";
