c27fd544808c1f6e6f523c8db1bd78328429a537
Greg Mahone <gmahone@gmail.com>
2022-09-30 11:42:47 +0200

    fix: working on charCodeArray

--- char_code_calculation.js
+++ char_code_calculation.js
@@ -1,5 +1,5 @@
 function calc(x){
-  let charCodeArray = String(x).split("").map(e => String(e.charCodeAt()));
+  let charCodeArray = String(x).split("").map(e => String(e.charCodeAt())).join("").split("");
   console.log(charCodeArray);
   
   return "";
