0b8f59e543548a4cb9f7d22c65804fbba9014d22
Greg Mahone <gmahone@gmail.com>
2022-09-30 13:10:37 +0200

    fix: remove console.logs

--- char_code_calculation.js
+++ char_code_calculation.js
@@ -3,16 +3,12 @@ function calc(x){
     .map(e => String(e.charCodeAt()))
     .join("")
     .split("");
-  console.log(charCodeArray)
   
-  let charCodeSwapArray = charCodeArray.map(a => a == "7" ? "1" : a)
-  console.log(charCodeSwapArray)
+  let charCodeSwapArray = charCodeArray.map(a => a == "7" ? "1" : a);
     
   let charCodeSum = charCodeArray.reduce((acc,c) => acc + +c, 0);
-  console.log(charCodeSum)
   
   let charCodeSwapSum = charCodeSwapArray.reduce((acc,c) => acc + +c, 0);
-  console.log(charCodeSwapSum);
-  
+
   return(charCodeSum - charCodeSwapSum);
 }
