71c84307f334c36551b2410cf7df0864a1a61184
Greg Mahone <gmahone@gmail.com>
2022-10-01 15:58:34 +0200

    feat: add console.logs

--- find_the_slope.js
+++ find_the_slope.js
@@ -1,5 +1,8 @@
 function slope(points){
   let deltaY = (points[3] - points[1]);
+  console.log(deltaY);
   let deltaX = (points[2] - points[0]);
+  console.log(deltaX);
+  console.log(deltaY/deltaX);
   return("");
 }
