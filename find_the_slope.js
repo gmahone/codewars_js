979b6e5b641051a360327f8f95d64e4cb76e697a
Greg Mahone <gmahone@gmail.com>
2022-10-01 16:01:47 +0200

    fix: remove console.logs

--- find_the_slope.js
+++ find_the_slope.js
@@ -1,9 +1,6 @@
 function slope(points){
   let deltaY = (points[3] - points[1]);
-  console.log(deltaY);
   let deltaX = (points[2] - points[0]);
-  console.log(deltaX);
-  console.log(deltaY/deltaX);
   let result = "undefined";
   if(deltaX !== 0){
     result = String(deltaY/deltaX);
