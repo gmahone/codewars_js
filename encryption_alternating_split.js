707d8cfa0532cfdb51aee9ba9f8a815014173de6
Greg Mahone <gmahone@gmail.com>
2022-10-02 16:18:13 +0200

    fix: remove console.logs from encrypt

--- encryption_alternating_split.js
+++ encryption_alternating_split.js
@@ -6,19 +6,14 @@ function encrypt(text, n) {
     let oddArray = [];
     let evenArray = [];
     for(let j = 0; j < text.length; j++){
-      console.log(text[j])
       if(j & 1){
         oddArray.push(text[j]);
       } else {
         evenArray.push(text[j]);
       }
     }
-    console.log(oddArray.join(""))
-    console.log(evenArray.join(""))
     text = oddArray.join("") + evenArray.join("")
-    console.log(text)
   }
-  console.log("\n\n")
   return(text);
 }
 
