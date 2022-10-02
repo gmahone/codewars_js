7929847a249e6ddda8a30236ff16732621bc829c
Greg Mahone <gmahone@gmail.com>
2022-10-02 15:58:48 +0200

    feat: encrypt function logic

--- encryption_alternating_split.js
+++ encryption_alternating_split.js
@@ -1,5 +1,15 @@
 function encrypt(text, n) {
-  
+  let oddArray;
+  let evenArray;
+  for(let i = 0; i < n; i++){
+    if(i & 1){
+      oddArray.push(text[i]);
+    } else {
+      evenArray.push(text[i]);
+    }
+    console.log(oddArray,evenArray);
+  }
+  result "";
 }
 
 function decrypt(encryptedText, n) {
