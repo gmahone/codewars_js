941c0a86db2235840651351e7ef6fc099621cc55
Greg Mahone <gmahone@gmail.com>
2022-10-04 20:25:50 +0200

    fix: conditionals

--- loop_statement.js
+++ loop_statement.js
@@ -1,8 +1,11 @@
 function giveMeFive(obj){
   let result = [];
   for(let key in obj){
-    if(obj[key].length > 4){
+    if(obj[key].length == 5){
       result.push(obj[key]);
+    }
+    if(key.length == 5){
+      result.push(key);
     }
   }
   return(result);
