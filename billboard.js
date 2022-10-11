339fd861f6c14b6f23ada4c5620dcfb5cd8d146b
Greg Mahone <gmahone@gmail.com>
2022-10-11 22:10:10 +0200

    feat: add solution using reduce

--- billboard.js
+++ billboard.js
@@ -5,3 +5,9 @@ function billboard(name, price = 30){
   }
   return(totalPrice);
 } 
+
+
+// using reduce
+function billboard(name, price = 30) {
+  return name.split('').reduce((sum, letter) => sum + price, 0); 
+}
