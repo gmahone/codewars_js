4ffa64db2de4f6bbafa08473474b54c00f3e290e
Greg Mahone <gmahone@gmail.com>
2022-10-13 22:37:53 +0200

    feat: initial solution

--- odd_even_string_sort.js
+++ odd_even_string_sort.js
@@ -2,6 +2,5 @@ function sortMyString(S) {
     splitString = S.split("");
     evenString = splitString.filter((e,i) => !(i & 1)).join("");
     oddString = splitString.filter((e,i) => i & 1).join("");
-    console.log(`${evenString} ${oddString}`);
-    return '';
+    return(`${evenString} ${oddString}`);
 }
