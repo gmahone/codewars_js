309225e08c824ede91410c9773510fdceb123c66
Greg Mahone <gmahone@gmail.com>
2022-10-13 22:38:39 +0200

    feat: add string addition solution

--- odd_even_string_sort.js
+++ odd_even_string_sort.js
@@ -4,3 +4,11 @@ function sortMyString(S) {
     oddString = splitString.filter((e,i) => i & 1).join("");
     return(`${evenString} ${oddString}`);
 }
+
+
+// string addition
+const sortMyString = s => {
+    let even = s.split('').filter((v, i) => i % 2 === 0).join('')
+    let odd = s.split('').filter((v, i) => i % 2 !== 0).join('')
+    return even + ' ' + odd
+}
