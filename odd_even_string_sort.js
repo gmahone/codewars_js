2bcada71fbadabdc969b402b9fa7a5f404674eec
Greg Mahone <gmahone@gmail.com>
2022-10-13 22:34:16 +0200

    feat: add splitting, even, and odd filtering

--- odd_even_string_sort.js
+++ odd_even_string_sort.js
@@ -1,3 +1,7 @@
 function sortMyString(S) {
+    splitString = S.split("");
+    evenIndices = splitString.filter((e,i) => !(i & 1));
+    oddIndices = splitString.filter((e,i) => i & 1);
+    console.log(evenIndices, oddIndices);
     return '';
 }
