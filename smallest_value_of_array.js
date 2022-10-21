1fbe7588d41a6e86fea7771d35e2a44b1fd676f1
Greg Mahone <gmahone@gmail.com>
2022-10-21 20:57:12 +0200

    feat: add default return

--- smallest_value_of_array.js
+++ smallest_value_of_array.js
@@ -2,8 +2,9 @@ function min(arr, toReturn) {
   minValue = Math.min(...arr);
   if(toReturn === "value"){
     return(minValue);
-  else if(toReturn === "index"){
+  } else if(toReturn === "index"){
     return(arr.indexOf(minValue));
+  } else {
+    return(undefined);
   }
-  return "";
 }
