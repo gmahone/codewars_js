7012c7ef75770b80649934ee957d034b5818262c
Greg Mahone <gmahone@gmail.com>
2022-10-21 20:56:33 +0200

    feat: add index return

--- smallest_value_of_array.js
+++ smallest_value_of_array.js
@@ -2,6 +2,8 @@ function min(arr, toReturn) {
   minValue = Math.min(...arr);
   if(toReturn === "value"){
     return(minValue);
-  else if(toReturn === "index")
+  else if(toReturn === "index"){
+    return(arr.indexOf(minValue));
+  }
   return "";
 }
