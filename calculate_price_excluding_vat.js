1c03524c2e004f941838579c1eebda63241c844a
Greg Mahone <gmahone@gmail.com>
2022-10-22 09:10:51 +0200

    fix: add console.logs to debug

--- calculate_price_excluding_vat.js
+++ calculate_price_excluding_vat.js
@@ -1,5 +1,8 @@
 function excludingVatPrice(price){
+  console.log(price)
   let priceWithoutVat = price / 1.15
+  console.log(priceWithoutVat)
   priceWithoutVat = Math.round(priceWithoutVat * 100)/100
+  console.log(priceWithoutVat)
   return priceWithoutVat;
 }
