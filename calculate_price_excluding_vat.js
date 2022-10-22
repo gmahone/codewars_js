81a21280fb7af41f0112f949250762e366ebde95
Greg Mahone <gmahone@gmail.com>
2022-10-22 09:14:12 +0200

    feat: add solution

--- calculate_price_excluding_vat.js
+++ calculate_price_excluding_vat.js
@@ -2,10 +2,7 @@ function excludingVatPrice(price){
   if(price === null){
     return(-1);
   }
-  console.log(price)
   let priceWithoutVat = price / 1.15
-  console.log(priceWithoutVat)
   priceWithoutVat = Math.round(priceWithoutVat * 100)/100
-  console.log(priceWithoutVat)
-  return priceWithoutVat;
+  return(priceWithoutVat);
 }
