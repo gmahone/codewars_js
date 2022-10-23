1ebff7c02758e2de0581723a1090dbd8c1d0fe63
Greg Mahone <gmahone@gmail.com>
2022-10-23 21:08:58 +0200

    feat: add solution using push of all products then max

--- maximum_product.js
+++ maximum_product.js
@@ -8,3 +8,12 @@ function adjacentElementsProduct(array) {
   }
   return(maxProduct);
 }
+
+// solution using push
+function adjacentElementsProduct(array) {
+  let newArr = []
+  for(i=0; i < array.length-1; i++){
+    newArr.push(array[i]*array[i+1])
+  }  
+  return Math.max(...newArr)
+}
