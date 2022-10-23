ba3e8309c8753cd5511f4506baf904ba47ce1641
Greg Mahone <gmahone@gmail.com>
2022-10-23 21:07:31 +0200

    feat: improve solution format

--- maximum_product.js
+++ maximum_product.js
@@ -1,6 +1,5 @@
 function adjacentElementsProduct(array) {
   let maxProduct = array[0] * array[1];
-  
   for(let i = 0; i < (array.length-1); i++){
     let tmpProduct = array[i] * array[i+1];
     if(tmpProduct > maxProduct){
