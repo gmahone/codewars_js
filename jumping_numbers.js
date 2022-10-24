0814581991942bc4056f6aa496bb7cc30561616c
Greg Mahone <gmahone@gmail.com>
2022-10-24 21:17:24 +0200

    feat: add edge case return

--- jumping_numbers.js
+++ jumping_numbers.js
@@ -1,5 +1,8 @@
 function jumpingNumber(n){
   let inputString = String(n).split("");
+  let result = "Jumping!!"
+  if(inputString.length == 1){
+    return(result);
   console.log(inputString);
   return "";
 }
