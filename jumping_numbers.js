fe11bf33b7f536119d7b8afcf5f061ffc7b4874d
Greg Mahone <gmahone@gmail.com>
2022-10-24 21:21:44 +0200

    fix: syntax

--- jumping_numbers.js
+++ jumping_numbers.js
@@ -4,7 +4,7 @@ function jumpingNumber(n){
   if(inputString.length == 1){
     return(result);
   }
-  for(let i == 0, i < (inputString.length-1); i++){
+  for(let i = 0; i < (inputString.length-1); i++){
     let absDiff = Math.abs(Number(inputString[i]) - Number(inputString[i+1]));
     if(absDiff !== 1){
       return("Not!!");
