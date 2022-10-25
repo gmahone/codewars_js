63a2a5a9b350cb3ddc8fa42cc6fbad5ad17cab5a
Greg Mahone <gmahone@gmail.com>
2022-10-25 21:34:40 +0200

    feat: add initial solution

--- grader.js
+++ grader.js
@@ -1,3 +1,16 @@
 function grader(score) {
-  return "";
+  let result = "F";
+  if(score >= 0.6){
+    result = "D";
+  }
+  if(score >= 0.7){
+    result = "C";
+  }
+  if(score >= 0.8){
+    result = "B";
+  }
+  if(score >= 0.9 && score <= 1.0){
+    result = "A";
+  }
+  return(result);
 }
