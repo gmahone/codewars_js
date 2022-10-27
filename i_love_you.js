f8f901a156bf982379c5af1ac03f9f6c0c17cb8b
Greg Mahone <gmahone@gmail.com>
2022-10-27 21:33:26 +0200

    feat: add different indexing solution

--- i_love_you.js
+++ i_love_you.js
@@ -3,3 +3,17 @@ function howMuchILoveYou(nbPetals) {
     let response = responseList[nbPetals % responseList.length];
     return(response);
 }
+
+
+// using different indexing
+const phrases = [
+    'I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all',
+]
+function howMuchILoveYou(n) {
+     return phrases[(n - 1) % phrases.length] 
+}
