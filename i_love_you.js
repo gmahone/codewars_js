37baf23c81b2c83577cd3695bf8f5269cbf0c97b
Greg Mahone <gmahone@gmail.com>
2022-10-27 21:29:00 +0200

    feat: initial solution

--- i_love_you.js
+++ i_love_you.js
@@ -1,7 +1,5 @@
 let responseList = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"];
-
 function howMuchILoveYou(nbPetals) {
     let response = responseList[(nbPetals % responseList.length) - 1];
-    console.log(response);
-    return "";
+    return(response);
 }
