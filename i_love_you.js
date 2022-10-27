157f3beee26b5e7f4a07d0967ec23dfb120619dd
Greg Mahone <gmahone@gmail.com>
2022-10-27 21:28:32 +0200

    feat: add function logic

--- i_love_you.js
+++ i_love_you.js
@@ -1,5 +1,7 @@
 let responseList = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"];
 
 function howMuchILoveYou(nbPetals) {
+    let response = responseList[(nbPetals % responseList.length) - 1];
+    console.log(response);
     return "";
 }
