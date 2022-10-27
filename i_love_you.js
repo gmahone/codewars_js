261bc126a150099e835f475b05caf9c28ac62182
Greg Mahone <gmahone@gmail.com>
2022-10-27 21:33:51 +0200

    feat: add solution using a map

--- i_love_you.js
+++ i_love_you.js
@@ -17,3 +17,17 @@ const phrases = [
 function howMuchILoveYou(n) {
      return phrases[(n - 1) % phrases.length] 
 }
+
+
+// using a map
+function howMuchILoveYou(nbPetals) {
+  let phrase = {    
+    0: "not at all",
+    1: "I love you",
+    2: "a little",
+    3: "a lot",
+    4: "passionately",
+    5: "madly"
+  }
+  return phrase[nbPetals%6]
+}
