42f60dcf6fa95723b495beed48b1c875fcf1809a
Greg Mahone <gmahone@gmail.com>
2022-10-29 09:38:30 +0200

    feat: add solution using this

--- terminal_game_1.js
+++ terminal_game_1.js
@@ -6,3 +6,13 @@ function Hero (name = "Hero") {
              experience: 0};
   return(hero);
 }
+
+
+// using this
+function Hero (name) {
+  this.name = name || 'Hero';
+  this.position = '00';
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
+}
