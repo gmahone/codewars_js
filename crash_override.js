6e1de1fcdd2144dbde5d9e558bb21878a7815397
Greg Mahone <gmahone@gmail.com>
2022-11-06 20:39:50 +0100

    feat: testing object

--- crash_override.js
+++ crash_override.js
@@ -1,6 +1,6 @@
-function aliasGen(firstName, surname){
-    firstLetter = firstName.substring(0,1);
+function aliasGen(first, surname){
+    firstLetter = first.substring(0,1);
     secondLetter = surname.substring(0,1);
-    console.log(firstLetter.toUpperCase(), secondLetter.toUpperCase());
+    console.log(firstName[firstLetter]);
     return "";
 }
