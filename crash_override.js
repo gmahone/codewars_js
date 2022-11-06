1cfd53fff784d14573c2fb488afb557eb76ad79c
Greg Mahone <gmahone@gmail.com>
2022-11-06 20:48:44 +0100

    feat: add first name object

--- crash_override.js
+++ crash_override.js
@@ -9,3 +9,32 @@ function aliasGen(first, last){
     console.log(result);
     return "";
 }
+
+/* first name object
+{ A: 'Alpha',
+  B: 'Beta',
+  C: 'Cache',
+  D: 'Data',
+  E: 'Energy',
+  F: 'Function',
+  G: 'Glitch',
+  H: 'Half-life',
+  I: 'Ice',
+  J: 'Java',
+  K: 'Keystroke',
+  L: 'Logic',
+  M: 'Malware',
+  N: 'Nagware',
+  O: 'OS',
+  P: 'Phishing',
+  Q: 'Quantum',
+  R: 'RAD',
+  S: 'Strike',
+  T: 'Trojan',
+  U: 'Ultraviolet',
+  V: 'Vanilla',
+  W: 'WiFi',
+  X: 'Xerox',
+  Y: 'Y',
+  Z: 'Zero' }
+  */
