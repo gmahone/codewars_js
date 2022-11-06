2705ff2c369be327aad0efe6564d284893ac9e74
Greg Mahone <gmahone@gmail.com>
2022-11-06 20:28:36 +0100

    feat: take substrings

--- crash_override.js
+++ crash_override.js
@@ -1,3 +1,6 @@
-function aliasGen(){
+function aliasGen(firstName, surname){
+    firstLetter = firstName.substring(0,1);
+    secondLetter = surname.substring(0,1);
+    console.log(firstLetter, secondLetter);
     return "";
 }
