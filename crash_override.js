0d0aeb66b6d8ccd551c58d9964974a787f810039
Greg Mahone <gmahone@gmail.com>
2022-11-06 20:36:17 +0100

    feat: add uppercase flip

--- crash_override.js
+++ crash_override.js
@@ -1,6 +1,6 @@
 function aliasGen(firstName, surname){
     firstLetter = firstName.substring(0,1);
     secondLetter = surname.substring(0,1);
-    console.log(firstLetter, secondLetter);
+    console.log(firstLetter.toUpperCase(), secondLetter.toUpperCase());
     return "";
 }
