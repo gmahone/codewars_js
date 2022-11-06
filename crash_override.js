35ef102b27920c39169c6d52918cbb45ae8d5297
Greg Mahone <gmahone@gmail.com>
2022-11-06 20:47:16 +0100

    feat: add full logic

--- crash_override.js
+++ crash_override.js
@@ -1,8 +1,11 @@
 function aliasGen(first, last){
     firstLetter = first.substring(0,1);
     secondLetter = last.substring(0,1);
-    console.log(firstName[firstLetter]);
-    console.log(surname[secondLetter]);
-    console.log(surname.hasOwnProperty(secondLetter))
+    let result = "Your name must start with a letter from A - Z.";
+    if(firstName.hasOwnProperty(firstLetter) && 
+       surname.hasOwnProperty(secondLetter)){
+        result = `${firstName[firstLetter]} ${surname[secondLetter]}`;
+    }
+    console.log(result);
     return "";
 }
