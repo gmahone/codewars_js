c591fd1ff1e3728a6ec824f22453f94644db0e8e
Greg Mahone <gmahone@gmail.com>
2022-11-06 20:52:27 +0100

    feat: add alternative solution

--- crash_override.js
+++ crash_override.js
@@ -67,3 +67,16 @@ function aliasGen(first, last){
   Y: 'Yob',
   Z: 'Zombie' }
   */
+
+
+// alternative solution
+function aliasGen(first,last){
+  let auxFirst = first[0].toUpperCase( )
+  let auxLast = last[0].toUpperCase( )
+
+  if(firstName[auxFirst] && surname[auxLast]) {
+    return firstName[auxFirst] + ' ' + surname[auxLast]
+  } else {
+    return 'Your name must start with a letter from A - Z.'
+  }
+}
