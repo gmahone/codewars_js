4c2dfe6228f73689818ea35bfebefd3a6b7e841a
Greg Mahone <gmahone@gmail.com>
2022-11-07 14:31:15 +0100

    feat: solution formatting

--- sum_of_odd_cubed_numbers.js
+++ sum_of_odd_cubed_numbers.js
@@ -2,13 +2,8 @@ function cubeOdd(arr) {
   if(arr.length !== arr.filter(e => Number.isInteger(e)).length){
     return(undefined);
   };
-  console.log(arr)
   let result = arr.map(e => e**3)
     .filter(e => e & 1)
-  console.log(result)
-
-  console.log(result)
   result = result.reduce((acc,e) => acc + e, 0);
-  console.log(result)
   return result;
 }
