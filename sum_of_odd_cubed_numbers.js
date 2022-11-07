1887c02928ccda048e476b782fa89122c2015133
Greg Mahone <gmahone@gmail.com>
2022-11-07 14:24:23 +0100

    feat: add edge case for alphabetic

--- sum_of_odd_cubed_numbers.js
+++ sum_of_odd_cubed_numbers.js
@@ -1,4 +1,7 @@
 function cubeOdd(arr) {
+  if(arr.length !== arr.filter(e => Number.isInteger(e)).length){
+    return(undefined);
+  };
   let result = arr.map(e => e**3)
     .filter(e => e & 1)
     .reduce((acc,e) => acc + e);
