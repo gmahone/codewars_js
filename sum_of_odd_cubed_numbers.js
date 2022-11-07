325c00fa68cfd61636e57ff8b8100b083eb1ab00
Greg Mahone <gmahone@gmail.com>
2022-11-07 14:29:51 +0100

    fix: broken return of 0 vs undefined

--- sum_of_odd_cubed_numbers.js
+++ sum_of_odd_cubed_numbers.js
@@ -2,12 +2,13 @@ function cubeOdd(arr) {
   if(arr.length !== arr.filter(e => Number.isInteger(e)).length){
     return(undefined);
   };
+  console.log(arr)
   let result = arr.map(e => e**3)
     .filter(e => e & 1)
-  if(result.length == 0){
-    return(undefined);
-  }
-  result.reduce((acc,e) => acc + e);
-  console.log(result);
+  console.log(result)
+
+  console.log(result)
+  result = result.reduce((acc,e) => acc + e, 0);
+  console.log(result)
   return result;
 }
