9fb8d968487cbf3bcae2b7ef0b7de8d9936c4306
Greg Mahone <gmahone@gmail.com>
2022-11-19 21:36:41 +0100

    fix: edge case return val

--- nth_digit_of_number.js
+++ nth_digit_of_number.js
@@ -2,7 +2,7 @@ var findDigit = function(num, nth){
     let numAsString = String(Math.abs(num));
     let newIndex = numAsString.length - nth
     if(newIndex < 0 || nth <= 0){
-      return(0);
+      return(-1);
     } else {
       let result = Number(numAsString.substring(newIndex, newIndex+1))
       return(result);
