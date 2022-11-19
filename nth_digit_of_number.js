b79b72dbf8e53f4599167933aa84a4da6c3d6b93
Greg Mahone <gmahone@gmail.com>
2022-11-19 21:35:40 +0100

    feat: initial solution

--- nth_digit_of_number.js
+++ nth_digit_of_number.js
@@ -1,6 +1,9 @@
 var findDigit = function(num, nth){
     let numAsString = String(Math.abs(num));
     let newIndex = numAsString.length - nth
-    
+    if(newIndex < 0 || nth < 0){
+        return(0);
+    } else {
+        return(numAsString.substring(newIndex, newIndex+1)
     return("");
 }
