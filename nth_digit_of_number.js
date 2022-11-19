167cfc7d3e678ae19f675e5305a43dd76645ea95
Greg Mahone <gmahone@gmail.com>
2022-11-19 21:36:24 +0100

    fix: control nth is positive

--- nth_digit_of_number.js
+++ nth_digit_of_number.js
@@ -1,7 +1,7 @@
 var findDigit = function(num, nth){
     let numAsString = String(Math.abs(num));
     let newIndex = numAsString.length - nth
-    if(newIndex < 0 || nth < 0){
+    if(newIndex < 0 || nth <= 0){
       return(0);
     } else {
       let result = Number(numAsString.substring(newIndex, newIndex+1))
