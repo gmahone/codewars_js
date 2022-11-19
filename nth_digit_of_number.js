5fc83dde29785169c10c5c13ecf59ad1d3677932
Greg Mahone <gmahone@gmail.com>
2022-11-19 21:36:10 +0100

    fix: clean up return type

--- nth_digit_of_number.js
+++ nth_digit_of_number.js
@@ -2,8 +2,10 @@ var findDigit = function(num, nth){
     let numAsString = String(Math.abs(num));
     let newIndex = numAsString.length - nth
     if(newIndex < 0 || nth < 0){
-        return(0);
+      return(0);
     } else {
-        return(numAsString.substring(newIndex, newIndex+1)
+      let result = Number(numAsString.substring(newIndex, newIndex+1))
+      return(result);
+    }
     return("");
 }
