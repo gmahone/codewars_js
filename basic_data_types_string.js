d6fcd4ecee63cc3a83244817118d7b55bc4af1e7
Greg Mahone <gmahone@gmail.com>
2022-11-25 20:52:32 +0100

    feat: fix first function

--- basic_data_types_string.js
+++ basic_data_types_string.js
@@ -1,7 +1,7 @@
 var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
 function Dad(){
   //select some variable to combine "Dad"
-  return ?+?+?;
+  return d1+a2+d2;
 }
 function Bee(){
   //select some variable to combine "Bee"
